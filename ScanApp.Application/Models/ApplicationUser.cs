7d7eaf25d881227da08151540e00de571c49b190
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-01 15:23:14 +0100

    Add basic structure, add updated user model

---
+++ ScanApp.Application/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ScanApp.Application.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Location { get; set; }
+    }
+}
