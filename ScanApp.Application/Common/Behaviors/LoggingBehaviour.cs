9761838f2b0830cad30a111baf9cac2dbe435a7f
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-02 22:04:13 +0100

    Add Logging behavior - all MediatR Commands / Queries will now be additionally logged (with params if possible)

---
+++ ScanApp.Application/Common/Behaviors/LoggingBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScanApp.Application.Common.Behaviors
+{
+    public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<LoggingBehaviour<TRequest, TResponse>> _logger;
+
+        public LoggingBehaviour(ILogger<LoggingBehaviour<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            //Request
+            _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+            var myType = request.GetType();
+            var props = new List<PropertyInfo>(myType.GetProperties());
+            foreach (var prop in props)
+            {
+                object propValue = prop.GetValue(request, null);
+                _logger.LogInformation("{Property} : {@Value}", prop.Name, propValue);
+            }
+            var response = await next();
+            //Response
+            _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+            return response;
+        }
+    }
+}
