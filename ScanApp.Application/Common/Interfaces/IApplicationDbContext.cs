bafe0342deffae39d29e7c36a16f7e066c0e212e
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-02 22:05:30 +0100

    Abstract ASP core security DbContext so it can be injected into services
    Since application layer cannot take db context directly, an abstraction with most common functionality has been made

---
+++ ScanApp.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using ScanApp.Application.Common.Entities;
+
+namespace ScanApp.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<IdentityUserRole<string>> UserRoles { get; set; }
+        DbSet<IdentityRole> Roles { get; set; }
+        DbSet<IdentityRoleClaim<string>> RoleClaims { get; set; }
+        DbSet<ApplicationUser> Users { get; set; }
+        DbSet<IdentityUserClaim<string>> UserClaims { get; set; }
+        DbSet<IdentityUserLogin<string>> UserLogins { get; set; }
+        DbSet<IdentityUserToken<string>> UserTokens { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+        int SaveChanges();
+
+        ChangeTracker ChangeTracker { get; }
+        event EventHandler<SavingChangesEventArgs> SavingChanges;
+        event EventHandler<SavedChangesEventArgs> SavedChanges;
+        event EventHandler<SaveChangesFailedEventArgs> SaveChangesFailed;
+    }
+}
