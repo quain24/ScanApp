aa73598a1c04e76f9a0710f7fbfcae6814f8830e
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-04 18:34:21 +0100

    Order security installer

--- ScanApp/Common/Installers/SecurityInstaller.cs
+++ ScanApp/Common/Installers/SecurityInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using ScanApp.Application.Common.Entities;
@@ -34,8 +35,19 @@ namespace ScanApp.Common.Installers
                 .AddRoleManager<RoleManager<IdentityRole>>()
                 .AddUserManager<UserManager<ApplicationUser>>();
 
-            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
+            // Enables immediate logout after refresh if user logged in on another session (zero interval is safe when using SignalR)
+            services.Configure<SecurityStampValidatorOptions>(options => options.ValidationInterval = TimeSpan.Zero);
 
+            // Login page is displayed if user is not authorized - also on startup
+            services.AddAuthorization(options =>
+            {
+                options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+
+            // Registers a service to refresh user authorization periodically - timespan is set inside of this service
+            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
             return services;
         }
     }
