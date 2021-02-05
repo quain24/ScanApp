a3a98a29eae4b3bfd3a6e50e6c52456def60af6b
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-05 14:21:58 +0100

    Move some logic to MeiatR Command and Query

---
+++ ScanApp.Application/Admin/Commands/ChangeUserSecurityStampCommand.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ScanApp.Application.Common.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ScanApp.Application.Admin.Commands
+{
+    public class ChangeUserSecurityStampCommand : IRequest<bool>
+    {
+        public string User { get; }
+
+        public ChangeUserSecurityStampCommand(string userName)
+        {
+            User = userName;
+        }
+    }
+
+    public class ChangeUserSecurityStampCommandHandler : IRequestHandler<ChangeUserSecurityStampCommand, bool>
+    {
+        private readonly IServiceScopeFactory _factory;
+
+        public ChangeUserSecurityStampCommandHandler(IServiceScopeFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task<bool> Handle(ChangeUserSecurityStampCommand request, CancellationToken cancellationToken)
+        {
+            using (var scope = _factory.CreateScope())
+            {
+                var manager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+                var user = await manager.FindByNameAsync("test");
+                var result = await manager.UpdateSecurityStampAsync(user);
+                return result.Succeeded;
+            }
+        }
+    }
+}
---
+++ ScanApp.Application/Admin/Queries/GetAllUsersQuery.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ScanApp.Application.Common.Entities;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ScanApp.Application.Admin.Queries
+{
+    public class GetAllUsersQuery : IRequest<IList<ApplicationUser>>
+    {
+    }
+
+    public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, IList<ApplicationUser>>
+    {
+        private readonly IServiceScopeFactory _factory;
+
+        public GetAllUsersQueryHandler(IServiceScopeFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task<IList<ApplicationUser>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            using (var scope = _factory.CreateScope())
+            {
+                var manager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+                var user = await manager.Users.ToListAsync(cancellationToken);
+                return user;
+            }
+        }
+    }
+}
