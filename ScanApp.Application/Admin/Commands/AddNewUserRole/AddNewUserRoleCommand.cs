fbe3bf0bd3a6bb8336d3489a0eaa70950d75188a
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-08 20:41:23 +0100

    Add new query bare-bone

---
+++ ScanApp.Application/Admin/Commands/AddNewUserRole/AddNewUserRoleCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ScanApp.Application.Common.Helpers;
+
+namespace ScanApp.Application.Admin.Commands.AddNewUserRole
+{
+    public class AddNewUserRoleCommand : IRequest<Result>
+    {
+    }
+}
