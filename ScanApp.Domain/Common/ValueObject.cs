13c19230f5993f602eb393249382273456532cc2
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-08 10:29:16 +0100

    Add base class to simplify creation of Value objects
    Every value object should derive from it, so consistency will be maintained
    Those value objects typically will be used in Entities, so later on a conversion when saving / loading
    will be needed.

---
+++ ScanApp.Domain/Common/ValueObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ScanApp.Domain.Common
+{
+    /// <summary>
+    /// Base class used to simplify creation of value objects
+    /// </summary>
+    public abstract class ValueObject
+    {
+        /// <summary>
+        /// Override with values used to compare equality
+        /// <para></para>When overriding remember to return values as a 'yield' return
+        /// </summary>
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            var valueObject = (ValueObject)obj;
+
+            return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Aggregate(1, (current, obj) =>
+                {
+                    unchecked
+                    {
+                        return current * 23 + (obj?.GetHashCode() ?? 0);
+                    }
+                });
+        }
+
+        public static bool operator ==(ValueObject a, ValueObject b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObject a, ValueObject b)
+        {
+            return !(a == b);
+        }
+    }
+}
