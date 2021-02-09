cbd4815655f2a024f57f8b5908508a2632d76559
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-09 11:12:37 +0100

    Add 'create new role' command and validation

--- ScanApp.Application/Admin/Commands/AddNewUserRole/AddNewUserRoleCommand.cs
+++ ScanApp.Application/Admin/Commands/AddNewUserRole/AddNewUserRoleCommand.cs
@@ -1,9 +1,40 @@
 ﻿using MediatR;
-using ScanApp.Application.Common.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using ScanApp.Application.Common.Helpers.Result;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ScanApp.Application.Admin.Commands.AddNewUserRole
 {
     public class AddNewUserRoleCommand : IRequest<Result>
     {
+        public string RoleName { get; private set; }
+
+        public AddNewUserRoleCommand(string roleName)
+        {
+            RoleName = roleName;
+        }
+    }
+
+    public class AddNewUserRoleHandler : IRequestHandler<AddNewUserRoleCommand, Result>
+    {
+        private readonly IServiceScopeFactory _factory;
+
+        public AddNewUserRoleHandler(IServiceScopeFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task<Result> Handle(AddNewUserRoleCommand request, CancellationToken cancellationToken)
+        {
+            using (var scope = _factory.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+                var result = await roleManager.CreateAsync(new IdentityRole(request.RoleName));
+                return result.Succeeded ? new Result(ResultType.Created) : new Result(ErrorType.NotValid, result.Errors.Select(e => e.Code + " | " + e.Description).ToArray());
+            }
+        }
     }
 }
---
+++ ScanApp.Application/Admin/Commands/AddNewUserRole/AddNewUserRoleValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace ScanApp.Application.Admin.Commands.AddNewUserRole
+{
+    public class AddNewUserRoleValidator : AbstractValidator<AddNewUserRoleCommand>
+    {
+        private Regex roleNameRegex = new Regex(@"^[\p{L}0-9\s\\.\\-\\_]+$");
+
+        public AddNewUserRoleValidator()
+        {
+            RuleFor(c => c.RoleName)
+                .NotEmpty()
+                .Must(c => !c.StartsWith(' ') && !c.EndsWith(' '))
+                .WithMessage("Role name cannot begin or end with whitespace")
+                .Matches(roleNameRegex)
+                .WithMessage("Role name format is not valid");
+        }
+    }
+}
