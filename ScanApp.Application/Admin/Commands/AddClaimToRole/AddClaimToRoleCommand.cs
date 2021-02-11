5300abb800f72629c881ee4a6252ec3e83daecce
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-11 11:48:30 +0100

    Make add claim to a role command more robust
    Before adding new claim command will check if such a claim is already added to
    given role with same value. If so, no duplication will be added.
    Command validator has been added.

--- ScanApp.Application/Admin/Commands/AddClaimToRole/AddClaimToRoleCommand.cs
+++ ScanApp.Application/Admin/Commands/AddClaimToRole/AddClaimToRoleCommand.cs
@@ -1,9 +1,10 @@
-﻿using System.Linq;
-using System.Security.Claims;
+﻿using System;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using ScanApp.Application.Common.Helpers.Result;
+using ScanApp.Application.Common.Interfaces;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,48 +14,56 @@ namespace ScanApp.Application.Admin.Commands.AddClaimToRole
     {
         public string RoleId { get; }
         public string NewClaimName { get; }
+        public string ClaimValue { get; }
 
-        public AddClaimToRoleCommand(string roleId, string newClaimName)
+        public AddClaimToRoleCommand(string roleId, string newClaimName, string claimValue)
         {
             RoleId = roleId;
             NewClaimName = newClaimName;
+            ClaimValue = claimValue;
         }
     }
 
     public class AddClaimToRoleCommandHandler : IRequestHandler<AddClaimToRoleCommand, Result>
     {
-        private readonly IServiceScopeFactory _factory;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IApplicationDbContext _context;
 
-        public AddClaimToRoleCommandHandler(IServiceScopeFactory factory)
+        public AddClaimToRoleCommandHandler(RoleManager<IdentityRole> roleManager, IApplicationDbContext context)
         {
-            _factory = factory;
+            _roleManager = roleManager;
+            _context = context;
         }
 
         public async Task<Result> Handle(AddClaimToRoleCommand request, CancellationToken cancellationToken)
         {
-            using (var scope = _factory.CreateScope())
+            var role = await _roleManager.FindByIdAsync(request.RoleId).ConfigureAwait(false);
+
+            if (role is null)
             {
-                var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
-
-                var role = await roleManager.FindByIdAsync(request.RoleId);
-
-                if (role is null)
-                {
-                    return new Result(ErrorType.NotFound, "Role not found!");
-                }
-
-                var claim = new IdentityRoleClaim<string>()
-                {
-                    ClaimType = request.NewClaimName,
-                    RoleId = role.Id,
-                    ClaimValue = "CanAdd"
-                };
-
-                var result = await roleManager.AddClaimAsync(role, claim.ToClaim());
-                return result.Succeeded
-                    ? new Result(ResultType.Created)
-                    : new Result(ErrorType.NotValid, result.Errors.Select(e => $"{e.Code} | {e.Description}"));
+                return new Result(ErrorType.NotFound, "Role not found!");
             }
+
+            if (await _context.RoleClaims
+                .Where(r => r.RoleId == role.Id &&
+                            string.Equals(r.ClaimType, request.NewClaimName) &&
+                            string.Equals(r.ClaimValue, request.ClaimValue))
+                .FirstOrDefaultAsync(cancellationToken) is not null)
+            {
+                return new Result(ErrorType.Duplicated, $"Role {role.Name} already have claim {request.NewClaimName} with value {request.ClaimValue}");
+            }
+
+            var claim = new IdentityRoleClaim<string>()
+            {
+                ClaimType = request.NewClaimName,
+                RoleId = role.Id,
+                ClaimValue = request.ClaimValue
+            };
+
+            var result = await _roleManager.AddClaimAsync(role, claim.ToClaim()).ConfigureAwait(false);
+            return result.Succeeded
+                ? new Result(ResultType.Created)
+                : new Result(ErrorType.NotValid, result.Errors.Select(e => $"{e.Code} | {e.Description}"));
         }
     }
 }
---
+++ ScanApp.Application/Admin/Commands/AddClaimToRole/AddClaimToRoleCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace ScanApp.Application.Admin.Commands.AddClaimToRole
+{
+    public class AddClaimToRoleCommandValidator : AbstractValidator<AddClaimToRoleCommand>
+    {
+        private readonly Regex _allowedCharsRegex = new Regex(@"^[\p{L}0-9\s\\.\-\\_]+$");
+
+        public AddClaimToRoleCommandValidator()
+        {
+            RuleFor(c => c.NewClaimName)
+                .NotEmpty()
+                .Must(c => !c.StartsWith(' ') && !c.EndsWith(' '))
+                .WithMessage("Role name cannot begin or end with whitespace")
+                .Matches(_allowedCharsRegex)
+                .WithMessage("Role name format is not valid");
+
+            RuleFor(c => c.RoleId)
+                .NotEmpty()
+                .Matches(_allowedCharsRegex)
+                .WithMessage("Role ID format is not valid");
+        }
+    }
+}
