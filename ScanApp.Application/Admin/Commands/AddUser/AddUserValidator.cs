69357adc0aa79bbdc679de66b43348251d3cf774
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-16 16:20:05 +0100

    Make Adding user validation more precise and readable.
    Property will be checked by only necessary amount of validators:
    If first fails, then no need to validate this property further
    
    - Updated radzen components

--- ScanApp.Application/Admin/Commands/AddUser/AddUserValidator.cs
+++ ScanApp.Application/Admin/Commands/AddUser/AddUserValidator.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using System.Xml.Linq;
-using FluentValidation;
+﻿using FluentValidation;
 using ScanApp.Common.Validators;
 
 namespace ScanApp.Application.Admin.Commands.AddUser
@@ -8,18 +6,26 @@ namespace ScanApp.Application.Admin.Commands.AddUser
     public class AddUserValidator : AbstractValidator<AddUserCommand>
     {
         private readonly MustContainOnlyLettersOrAllowedSymbolsValidator _standardChars = new();
+
         public AddUserValidator()
         {
-            RuleFor(c => c.NewUser.Email)
-                .SetValidator(new EmailValidator());
-            RuleFor(c => c.NewUser.Location)
-                .SetValidator(_standardChars);
             RuleFor(c => c.NewUser.Name)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
                 .SetValidator(_standardChars);
             RuleFor(c => c.NewUser.Password)
                 .NotEmpty();
+            RuleFor(c => c.NewUser.Email)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .SetValidator(new EmailValidator());
+            RuleFor(c => c.NewUser.Location)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .SetValidator(_standardChars);
             RuleFor(c => c.NewUser.Phone)
-                .SetValidator(new PhoneNumberValidator());
+                .SetValidator(new PhoneNumberValidator())
+                .When(p => p.NewUser.Phone is not null);
         }
     }
 }
