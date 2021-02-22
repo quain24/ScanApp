a65a1aa79ecc1d9234deb19f6efc970dca8026fc
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-22 10:55:34 +0100

    Move UserNotFound method to outer class, so it can be used with more objects (dry)

---
+++ ScanApp.Infrastructure/Identity/ResultHelpers.cs
@@ -0,0 +1,9 @@
+﻿using ScanApp.Application.Common.Helpers.Result;
+
+namespace ScanApp.Infrastructure.Identity
+{
+    internal static class ResultHelpers
+    {
+        internal static Result UserNotFound(string userName) => new(ErrorType.NotFound, $"No user with name of {userName} found.");
+    }
+}
