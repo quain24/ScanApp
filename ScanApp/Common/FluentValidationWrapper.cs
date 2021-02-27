daf2c209119c577b3a76ba3df846dc3cf5708fb9
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-02-28 00:02:01 +0100

    Add functionality that enables usage of fluid validators
    This simple class enables usage of fluid validation from gui when
    utilizing MudBlazor components.

---
+++ ScanApp/Common/FluentValidationWrapper.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScanApp.Common
+{
+    public class FluentValidationWrapper<T> : AbstractValidator<T>
+    {
+        public FluentValidationWrapper(Action<IRuleBuilderInitial<T, T>> rule)
+        {
+            rule(RuleFor(x => x));
+        }
+
+        private IEnumerable<string> ValidateValue(T arg)
+        {
+            var result = Validate(arg);
+            return result.IsValid
+                ? Array.Empty<string>()
+                : result.Errors.Select(e => e.ErrorMessage);
+        }
+
+        public Func<T, IEnumerable<string>> Validation => ValidateValue;
+    }
+}
