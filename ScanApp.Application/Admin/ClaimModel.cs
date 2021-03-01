ead2e10439051ecaf8b527ccbeaf0c0550521895
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-03-01 16:03:45 +0100

    Add new models in preparation to introduce simple concurrency check
    Users will be monitored for concurrent modification / stale data
    Claim model introduced to simplify logic in razor components

---
+++ ScanApp.Application/Admin/ClaimModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ScanApp.Application.Admin
+{
+    public class ClaimModel : IEquatable<ClaimModel>
+    {
+        public string Type { get; }
+        public string Value { get; }
+
+        public ClaimModel(string type, string value)
+        {
+            Type = string.IsNullOrWhiteSpace(type)
+                ? throw new ArgumentOutOfRangeException(nameof(type), "Type must not be null, empty or whitespace-only")
+                : type;
+
+            if (value is not null && string.IsNullOrWhiteSpace(value))
+                throw new ArgumentOutOfRangeException(nameof(value), "If value is present, it must not be empty or contain only whitespaces");
+
+            Value = value;
+        }
+
+        public bool Equals(ClaimModel other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Type == other.Type && Value == other.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == this.GetType() && Equals((ClaimModel) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Type, Value);
+        }
+    }
+}
---
+++ ScanApp.Application/Admin/ConcurrencyStamp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ScanApp.Application.Admin
+{
+    public sealed class ConcurrencyStamp
+    {
+        public string Value { get; }
+
+        public ConcurrencyStamp(string value)
+        {
+            if (value is not null && string.IsNullOrWhiteSpace(value))
+                throw new FormatException($"{nameof(value)} must be either null or contain actual symbols");
+
+            Value = value;
+        }
+
+        public override string ToString() => Value;
+    }
+}
