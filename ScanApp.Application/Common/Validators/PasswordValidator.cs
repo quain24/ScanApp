07745860a610b15409c247445217cfa01bef5722
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-03-04 14:47:35 +0100

    Add password fluent password validator
    This validator is using asp identity configured ms validators.
    Thanks to that, all password validations will be performed
    in connection to rules set for asp identity in 'SecurityInstaller'

---
+++ ScanApp.Application/Common/Validators/PasswordValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using ScanApp.Application.Common.Interfaces;
+using System;
+
+namespace ScanApp.Application.Common.Validators
+{
+    public class PasswordValidator : AbstractValidator<string>
+    {
+        public PasswordValidator(IUserManager userManager)
+        {
+            _ = userManager ?? throw new ArgumentNullException(nameof(userManager));
+
+            RuleFor(s => s)
+                .CustomAsync(async (pass, context, _) =>
+                {
+                    var results = await userManager.ValidatePassword(pass).ConfigureAwait(false);
+                    foreach (var (code, message) in results)
+                    {
+                        context.AddFailure(new ValidationFailure(string.Empty, message)
+                        {
+                            ErrorCode = code
+                        });
+                    }
+                });
+        }
+
+        protected override bool PreValidate(ValidationContext<string> context, ValidationResult result)
+        {
+            if (context.InstanceToValidate is not null)
+                return true;
+
+            result.Errors.Add(new ValidationFailure(string.Empty, "Provided password was NULL"));
+            return false;
+        }
+    }
+}
