39309627827d23c6fad1858b29e3090bfcb29251
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-03-04 14:38:43 +0100

    Add extensions for fluent validation usage in mud blazor forms

---
+++ ScanApp/Common/Extensions/AbstractValidatorExtensions.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ScanApp.Common.Extensions
+{
+    public static class AbstractValidatorExtensions
+    {
+        public static Func<string, Task<IEnumerable<string>>> ToAsyncMudFormFieldValidator(this AbstractValidator<string> validator)
+        {
+            return async value =>
+            {
+                var res = await validator.ValidateAsync(value);
+                if (res.IsValid)
+                    return Array.Empty<string>();
+
+                var errors = new List<string>(res.Errors.Count);
+                foreach (var result in res.Errors)
+                {
+                    // TODO - Translation service here or directly in validator?
+                    errors.Add(result.ErrorMessage);
+                }
+                return errors;
+            };
+        }
+
+        public static Func<string, IEnumerable<string>> ToMudFormFieldValidator(this AbstractValidator<string> validator)
+        {
+            return value =>
+            {
+                var res = validator.Validate(value);
+                if (res.IsValid)
+                    return Array.Empty<string>();
+
+                var errors = new List<string>(res.Errors.Count);
+                foreach (var result in res.Errors)
+                {
+                    errors.Add(result.ErrorMessage);
+                }
+                return errors;
+            };
+        }
+    }
+}
