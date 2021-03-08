d21e27becb6b55640bbc7e66ef3c35fd6c8bb34d
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-03-08 17:04:17 +0100

    Move default snackbar config into installer

--- ScanApp/Common/Installers/MudBlazorInstaller.cs
+++ ScanApp/Common/Installers/MudBlazorInstaller.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
 using MudBlazor.Services;
 
 namespace ScanApp.Common.Installers
@@ -7,7 +8,16 @@ namespace ScanApp.Common.Installers
     {
         public static IServiceCollection AddMudBlazor(this IServiceCollection services)
         {
-            services.AddMudServices();
+            services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.ShowCloseIcon = true;
+                config.SnackbarConfiguration.VisibleStateDuration = 5000;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+            });
 
             return services;
         }
