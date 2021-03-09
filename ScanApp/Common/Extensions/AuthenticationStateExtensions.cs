e47f2498db3f6c379fde3e7eb2dddaba9faca7a5
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-03-09 20:32:58 +0100

    Add simplified way to check if given name is the currently logged-in one

---
+++ ScanApp/Common/Extensions/AuthenticationStateExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Security.Claims;
+
+namespace ScanApp.Common.Extensions
+{
+    public static class AuthenticationStateExtensions
+    {
+        public static bool IsItMe(this AuthenticationState state, string userName, string userNameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")
+        {
+            var identityName = state?.User?.FindFirstValue(userNameClaimType)
+                               ?? throw new ArgumentNullException(nameof(state), $"Either {nameof(AuthenticationState)}, there is no \"User\" in this {nameof(AuthenticationState)} or no value for passed {nameof(userNameClaimType)} was found.");
+            return string.Equals(identityName, userName, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
