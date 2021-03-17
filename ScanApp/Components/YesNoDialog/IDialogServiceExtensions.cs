f0160d985382ac0dc590ae97d95bce4c8496deb7
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-03-17 13:26:56 +0100

    Refactor confirmation dialog generation
    This dialog can now be created from extension methd, with
    automatically applied default options and simplified header / content
    text insertion.

---
+++ ScanApp/Components/YesNoDialog/IDialogServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+
+namespace ScanApp.Components.YesNoDialog
+{
+    public static class IDialogServiceExtensions
+    {
+        public static IDialogReference AskForConfirmation(this IDialogService dialogService, string title, string content, DialogOptions options = default)
+        {
+            options ??= new DialogOptions { CloseButton = false, DisableBackdropClick = true, FullWidth = true };
+            return dialogService.Show<YesNoDialog>(title, new DialogParameters { ["Content"] = content }, options);
+        }
+    }
+}
