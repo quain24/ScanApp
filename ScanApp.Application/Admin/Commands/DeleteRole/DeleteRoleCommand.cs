25ce16dee7d36599fe5f137fd017c8c4d90d6711
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-03-18 10:42:59 +0100

    Add role create and delete functions to admin panel

---
+++ ScanApp.Application/Admin/Commands/DeleteRole/DeleteRoleCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using ScanApp.Application.Common.Helpers.Result;
+using ScanApp.Application.Common.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScanApp.Application.Admin.Commands.DeleteRole
+{
+    public class DeleteRoleCommand : IRequest<Result>
+    {
+        public string RoleName { get; }
+
+        public DeleteRoleCommand(string roleName)
+        {
+            RoleName = roleName;
+        }
+
+        public class DeleteRoleCommandHandler : IRequestHandler<DeleteRoleCommand, Result>
+        {
+            private readonly IRoleManager _roleManager;
+
+            public DeleteRoleCommandHandler(IRoleManager roleManager)
+            {
+                _roleManager = roleManager;
+            }
+
+            public Task<Result> Handle(DeleteRoleCommand request, CancellationToken cancellationToken)
+            {
+                return _roleManager.RemoveRole(request.RoleName);
+            }
+        }
+    }
+}
