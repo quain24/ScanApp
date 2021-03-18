f916ebc5df7431414b73ea42431ce4cf43bf230f
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-03-18 10:41:17 +0100

    Remove custom yes-no dialog in favor of built-in one
    Built one is a bit more cumbersome to use, but requires
    one less level of abstraction. I've added a new Globals class
    that will serve constant application-wide values, including
    default dialog options
    
    - new 'globals' framework

---
+++ ScanApp/Globals.cs
@@ -0,0 +1,12 @@
+﻿using MudBlazor;
+
+namespace ScanApp
+{
+    public static partial class Globals
+    {
+        public static class Gui
+        {
+            public static DialogOptions DefaultDialogOptions => new() { CloseButton = false, FullWidth = true, DisableBackdropClick = true };
+        }
+    }
+}
