23c13654a6ad6c39472b392a5b6eef968ef33272
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-04-06 14:53:08 +0200

    Add tests for spare part types and spare part entities

---
+++ ScanApp.Tests/UnitTests/Domain/Entities/SparePartTypeTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using ScanApp.Domain.Entities;
+using System;
+using Xunit;
+
+namespace ScanApp.Tests.UnitTests.Domain.Entities
+{
+    public class SparePartTypeTests
+    {
+        [Fact]
+        public void Create_spare_part_type()
+        {
+            var subject = new SparePartType("name");
+
+            subject.Name.Should().BeEquivalentTo("name");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Name_cannot_be_empty(string name)
+        {
+            Action act = () => new SparePartType(name);
+
+            act.Should().Throw<ArgumentOutOfRangeException>("name cannot be null or empty");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Name_cannot_be_empty_when_set_by_property(string name)
+        {
+            var subject = new SparePartType("name");
+            Action act = () => subject.Name = name;
+
+            act.Should().Throw<ArgumentOutOfRangeException>("name cannot be null or empty");
+        }
+    }
+}
