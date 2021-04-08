d73b4a9c82e1694550aa9588b015d5ef93676001
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-04-08 18:39:59 +0200

    Add partial tests for UserInfoService
    - updated UserManager fixture - now it provides Users prop
    - updated nugets

---
+++ ScanApp.Tests/UnitTests/Infrastructure/Identity/UserInfoServiceTests.cs
@@ -0,0 +1,226 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using ScanApp.Application.Common.Entities;
+using ScanApp.Application.Common.Interfaces;
+using ScanApp.Infrastructure.Identity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Version = ScanApp.Domain.ValueObjects.Version;
+
+namespace ScanApp.Tests.UnitTests.Infrastructure.Identity
+{
+    public class UserInfoServiceTests
+    {
+        [Fact]
+        public void Will_create_UserInfoService()
+        {
+            var userManagerMock = UserManagerFixture.MockUserManager(new List<ApplicationUser>(0));
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            sut.Should().NotBeNull()
+                .And
+                .BeOfType<UserInfoService>()
+                .And
+                .BeAssignableTo<IUserInfo>();
+        }
+
+        [Fact]
+        public void Will_throw_arg_null_if_provided_user_manager_was_null()
+        {
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            Action Act = () => new UserInfoService(claimsFacMock.Object, null, userManagerServiceMock.Object);
+
+            Act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Will_throw_arg_null_if_provided_claims_factory_was_null()
+        {
+            var userManagerMock = UserManagerFixture.MockUserManager(new List<ApplicationUser>(0));
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            Action Act = () => new UserInfoService(null, userManagerMock.Object, userManagerServiceMock.Object);
+
+            Act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Will_throw_arg_null_if_provided_user_manager_service_was_null()
+        {
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerMock = UserManagerFixture.MockUserManager(new List<ApplicationUser>(0));
+
+            Action Act = () => new UserInfoService(claimsFacMock.Object, userManagerMock.Object, null);
+
+            Act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task UserExists_returns_true_if_user_exists()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.UserExists(user.UserName);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task UserExists_returns_false_if_no_user_with_given_name_exists()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.UserExists("other_name");
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task GetUserNameById_get_name_if_id_is_found()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.GetUserNameById(user.Id);
+
+            result.Should().Be(user.UserName);
+        }
+
+        [Fact]
+        public async Task GetUserNameById_returns_null_if_id_is_not_found()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.GetUserNameById("unknown_id");
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetUserIdByName_get_name_if_name_is_found()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.GetUserIdByName(user.UserName);
+
+            result.Should().Be(user.Id);
+        }
+
+        [Fact]
+        public async Task GetUserIdByName_returns_null_if_name_is_not_found()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.GetUserIdByName("unknown_name");
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetUserConcurrencyStamp_get_concurrency_stamp_if_name_is_found()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.GetUserConcurrencyStamp(user.UserName);
+
+            result.Should().Be(user.ConcurrencyStamp);
+        }
+
+        [Fact]
+        public async Task  GetUserConcurrencyStamp_returns_null_if_name_is_not_found()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.GetUserConcurrencyStamp("unknown_name");
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetUserVersion_returns_proper_version_if_name_is_found()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.GetUserVersion(user.UserName);
+
+            result.Should().Be(Version.Create(user.ConcurrencyStamp));
+        }
+
+        [Fact]
+        public async Task  GetUserVersion_returns_empty_version_if_name_is_not_found()
+        {
+            var user = UserGeneratorFixture.CreateValidUser();
+            var userList = new List<ApplicationUser> { user };
+            var userManagerMock = UserManagerFixture.MockUserManager(userList);
+            var claimsFacMock = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+            var userManagerServiceMock = new Mock<IUserManager>();
+
+            var sut = new UserInfoService(claimsFacMock.Object, userManagerMock.Object, userManagerServiceMock.Object);
+
+            var result = await sut.GetUserVersion("unknown_name");
+
+            result.Should().Be(Version.Empty());
+        }
+    }
+}
--- ScanApp.Tests/UnitTests/Infrastructure/Identity/UserManagerFixture.cs
+++ ScanApp.Tests/UnitTests/Infrastructure/Identity/UserManagerFixture.cs
@@ -2,6 +2,7 @@
 using Moq;
 using System.Collections.Generic;
 using System.Linq;
+using MockQueryable.Moq;
 using ScanApp.Application.Common.Entities;
 
 namespace ScanApp.Tests.UnitTests.Infrastructure.Identity
@@ -10,10 +11,13 @@ namespace ScanApp.Tests.UnitTests.Infrastructure.Identity
     {
         public static Mock<UserManager<TUser>> MockUserManager<TUser>(List<TUser> ls, IdentityResult deleteResult = null, IdentityResult createResult = null, IdentityResult updateResult = null, TUser findByNameResult = null) where TUser : class
         {
+            ls ??= new List<TUser>(0);
             var store = new Mock<IUserStore<TUser>>();
             var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, null, null, null, null, null, null);
             mgr.Object.UserValidators.Add(new UserValidator<TUser>());
             mgr.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+            var queryableList = ls.AsQueryable().BuildMock();
+            mgr.SetupGet(p => p.Users).Returns(queryableList.Object);
 
             mgr.Setup(x => x.DeleteAsync(It.IsAny<TUser>())).ReturnsAsync(deleteResult ?? IdentityResult.Success);
             mgr.Setup(x => x.CreateAsync(It.IsAny<TUser>(), It.IsAny<string>())).ReturnsAsync(createResult ?? IdentityResult.Success).Callback<TUser, string>((x, y) => ls.Add(x));
