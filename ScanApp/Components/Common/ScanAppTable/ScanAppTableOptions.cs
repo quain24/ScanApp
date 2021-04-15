b382e2590069eca7f485a87424c4b113813dcd1b
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-04-15 08:34:40 +0200

    Add SparePart page with functioning ScanAppTable

---
+++ ScanApp/Components/Common/ScanAppTable/ScanAppTableOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace ScanApp.Components.Common.ScanAppTable
+{
+    public class ScanAppTableOptions
+    {
+        public bool ShowToolBar { get; set; } = true;
+        public bool AllowGrouping { get; set; } = true;
+        public bool AllowFiltering { get; set; } = true;
+        public bool AllowMultiSelection { get; set; } = true;
+    }
+}
