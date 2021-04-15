ec93842e143a7d96fa57f0e218c16090f9a12c70
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-04-15 08:34:40 +0200

    Add SparePart page with functioning ScanAppTable

---
+++ ScanApp/Models/SpareParts/SparePartGUIModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScanApp.Models.SpareParts
+{
+    public class SparePartGUIModel
+    {
+        public SparePartGUIModel(string name, int amount)
+        {
+            Name = name;
+            Amount = amount;
+        }
+
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
