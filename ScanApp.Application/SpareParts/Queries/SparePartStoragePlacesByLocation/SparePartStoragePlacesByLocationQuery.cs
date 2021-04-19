a28ad4c1806f83045695fac52109feb20d1dd3c3
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-04-19 13:31:27 +0200

    Add SparePartStoragePlacesByLocationQueryHandler tests
    Added validator for query, so it wont allow empty or null location id's - less
    pressure for db.
    Added tests for validator

--- ScanApp.Application/SpareParts/Queries/SparePartStoragePlacesByLocation/SparePartStoragePlacesByLocationQuery.cs
+++ ScanApp.Application/SpareParts/Queries/SparePartStoragePlacesByLocation/SparePartStoragePlacesByLocationQuery.cs
@@ -1,27 +1,27 @@
-﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
-using ScanApp.Application.Common.Helpers.Result;
-using ScanApp.Application.Common.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ScanApp.Application.Common.Helpers.Result;
+using ScanApp.Application.Common.Interfaces;
 
-namespace ScanApp.Application.SpareParts.Queries.StoragePlacesByLocation
+namespace ScanApp.Application.SpareParts.Queries.SparePartStoragePlacesByLocation
 {
-    public record StoragePlacesByLocationQuery(string LocationId) : IRequest<Result<List<RepairWorkshopModel>>>;
+    public record SparePartStoragePlacesByLocationQuery(string LocationId) : IRequest<Result<List<RepairWorkshopModel>>>;
 
-    internal class StoragePlacesByLocationQueryHandler : IRequestHandler<StoragePlacesByLocationQuery, Result<List<RepairWorkshopModel>>>
+    internal class SparePartStoragePlacesByLocationQueryHandler : IRequestHandler<SparePartStoragePlacesByLocationQuery, Result<List<RepairWorkshopModel>>>
     {
         private readonly IContextFactory _contextFactory;
 
-        public StoragePlacesByLocationQueryHandler(IContextFactory contextFactory)
+        public SparePartStoragePlacesByLocationQueryHandler(IContextFactory contextFactory)
         {
-            _contextFactory = contextFactory;
+            _contextFactory = contextFactory ?? throw new ArgumentNullException(nameof(contextFactory));
         }
 
-        public async Task<Result<List<RepairWorkshopModel>>> Handle(StoragePlacesByLocationQuery request, CancellationToken cancellationToken)
+        public async Task<Result<List<RepairWorkshopModel>>> Handle(SparePartStoragePlacesByLocationQuery request, CancellationToken cancellationToken)
         {
             await using var ctx = _contextFactory.CreateDbContext();
             try
---
+++ ScanApp.Application/SpareParts/Queries/SparePartStoragePlacesByLocation/SparePartStoragePlacesByLocationQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ScanApp.Application.SpareParts.Queries.SparePartStoragePlacesByLocation
+{
+    internal class SparePartStoragePlacesByLocationQueryValidator : AbstractValidator<SparePartStoragePlacesByLocationQuery>
+    {
+        public SparePartStoragePlacesByLocationQueryValidator()
+        {
+            RuleFor(e => e.LocationId)
+                .NotEmpty()
+                .WithMessage("{PropertyName} cannot be null or empty or contain only whitespaces");
+        }
+    }
+}
