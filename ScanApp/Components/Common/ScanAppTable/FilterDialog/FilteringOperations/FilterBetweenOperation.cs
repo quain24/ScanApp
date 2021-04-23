dd407029ab2329aaae475cb359e58e700d11450d
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-04-23 16:09:15 +0200

    Major ScanAppTable refactor
    - add FilterDialogFieldGenerator.razor for clearer generation of fields in the filter dialog
    - add IFilteringOperations interface and 3 different filtering operations that implement the interface and a Factory
    - add extension methods for filtering, grouping operations on the collection in the ScanAppTable
    - add ItemCloner to enable EditDialog to work on a cloned TItem, not on a referece (the feature still is not working properly but is commited for other developers to work on)
    - change naming of most of the classes and variables
    - boosted performance - reflection use is minimal
    - add GroupingToGroupConverter for clearer distinction between IGrouping and Group.cs

---
+++ ScanApp/Components/Common/ScanAppTable/FilterDialog/FilteringOperations/FilterBetweenOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ScanApp.Components.Common.ScanAppTable.Extensions;
+
+namespace ScanApp.Components.Common.ScanAppTable.FilterDialog.FilteringOperations
+{
+    public class FilterBetweenOperation : IFilteringOperation
+    {
+        private string _propertyName;
+        private int? _from;
+        private int? _to;
+
+        public FilterBetweenOperation(string propertyName, int? from, int? to)
+        {
+            _propertyName = propertyName;
+            _from = from;
+            _to = to;
+        }
+
+        public string PropertyName
+        {
+            get => _propertyName;
+            set => _propertyName = value;
+        }
+
+        public IEnumerable<T> Run<T>(IEnumerable<T> items)
+        {
+            return items.FilterBetween(_propertyName, _from, _to);
+        }
+    }
+}
---
+++ ScanApp/Components/Common/ScanAppTable/FilterDialog/FilteringOperations/FilteringBetweenDatesOperation.cs
@@ -0,0 +1,31 @@
+﻿using ScanApp.Components.Common.ScanAppTable.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace ScanApp.Components.Common.ScanAppTable.FilterDialog.FilteringOperations
+{
+    public class FilteringBetweenDatesOperation : IFilteringOperation
+    {
+        private string _propertyName;
+        private DateTime? _fromDate;
+        private DateTime? _toDate;
+
+        public FilteringBetweenDatesOperation(string propertyName, DateTime? fromDate, DateTime? toDate)
+        {
+            _propertyName = propertyName;
+            _fromDate = fromDate;
+            _toDate = toDate;
+        }
+
+        public string PropertyName
+        {
+            get => _propertyName;
+            set => _propertyName = value;
+        }
+
+        public IEnumerable<T> Run<T>(IEnumerable<T> items)
+        {
+            return items.FilterBetweenDates(_propertyName, _fromDate, _toDate);
+        }
+    }
+}
