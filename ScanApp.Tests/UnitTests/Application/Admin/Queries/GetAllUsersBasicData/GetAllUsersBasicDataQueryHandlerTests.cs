f55a6fa652ad14865b6cc1164ff887a57e390140
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-04-26 12:32:44 +0200

    Add GetAllUsersBasicDataQueryHandler tests

---
+++ ScanApp.Tests/UnitTests/Application/Admin/Queries/GetAllUsersBasicData/GetAllUsersBasicDataQueryHandlerTests.cs
@@ -0,0 +1,90 @@
+﻿using FluentAssertions;
+using MediatR;
+using MockQueryable.Moq;
+using ScanApp.Application.Admin.Queries.GetAllUsersBasicData;
+using ScanApp.Application.Common.Helpers.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ScanApp.Application.Common.Entities;
+using Xunit;
+using Version = ScanApp.Domain.ValueObjects.Version;
+
+namespace ScanApp.Tests.UnitTests.Application.Admin.Queries.GetAllUsersBasicData
+{
+    public class GetAllUsersBasicDataQueryHandlerTests : IContextFactoryMockFixtures
+    {
+        [Fact]
+        public void Creates_instance()
+        {
+            var subject = new GetAllUsersBasicDataQueryHandler(ContextFactoryMock.Object);
+
+            subject.Should().BeOfType<GetAllUsersBasicDataQueryHandler>()
+                .And.BeAssignableTo(typeof(IRequestHandler<,>));
+        }
+
+        [Fact]
+        public void Throws_arg_null_exc_when_missing_IContextFactory()
+        {
+            Action act = () => _ = new GetAllUsersBasicDataQueryHandler(null);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task Throws_if_exception_was_thrown_when_receiving_data()
+        {
+            ContextFactoryMock.Setup(c => c.CreateDbContext()).Throws<ArgumentNullException>();
+
+            var subject = new GetAllUsersBasicDataQueryHandler(ContextFactoryMock.Object);
+            Func<Task> act = async () => await subject.Handle(new GetAllUsersBasicDataQuery(), CancellationToken.None);
+
+            await act.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task Returns_invalid_result_of_cancelled_if_cancellation_occurred()
+        {
+            var token = new CancellationTokenSource(0).Token;
+            ContextMock.SetupGet(c => c.Users).Throws<OperationCanceledException>();
+
+            var subject = new GetAllUsersBasicDataQueryHandler(ContextFactoryMock.Object);
+            var result = await subject.Handle(new GetAllUsersBasicDataQuery(), token);
+
+            result.Conclusion.Should().BeFalse();
+            result.ErrorDescription.ErrorType.Should().Be(ErrorType.Cancelled);
+            result.ErrorDescription.Exception.Should().BeOfType<OperationCanceledException>();
+        }
+
+        [Fact]
+        public async Task Returns_valid_result_with_data()
+        {
+            var users = UserGeneratorFixture.CreateValidListOfUsers();
+            var usersMock = users.AsQueryable().BuildMockDbSet();
+            ContextMock.Setup(m => m.Users).Returns(usersMock.Object);
+            var subject = new GetAllUsersBasicDataQueryHandler(ContextFactoryMock.Object);
+
+            var result = await subject.Handle(new GetAllUsersBasicDataQuery(), CancellationToken.None);
+
+            result.Conclusion.Should().BeTrue();
+            result.Output.Should().Equal(users, (resultModel, originalUser) =>
+                resultModel.Version.Equals(Version.Create(originalUser.ConcurrencyStamp)) &&
+                resultModel.Name.Equals(originalUser.UserName));
+        }
+
+        [Fact]
+        public async Task Returns_valid_empty_result_if_there_is_no_data()
+        {
+            var usersMock = new List<ApplicationUser>().AsQueryable().BuildMockDbSet();
+            ContextMock.Setup(m => m.Users).Returns(usersMock.Object);
+            var subject = new GetAllUsersBasicDataQueryHandler(ContextFactoryMock.Object);
+
+            var result = await subject.Handle(new GetAllUsersBasicDataQuery(), CancellationToken.None);
+
+            result.Conclusion.Should().BeTrue();
+            result.Output.Should().BeEmpty();
+        }
+    }
+}
---
+++ ScanApp.Tests/UnitTests/UserGeneratorFixture.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using ScanApp.Application.Common.Entities;
+
+namespace ScanApp.Tests.UnitTests
+{
+    public class UserGeneratorFixture
+    {
+        public static ApplicationUser CreateValidUser()
+        {
+            var user = new ApplicationUser()
+            {
+                ConcurrencyStamp = "123456",
+                Email = "user@domain.com",
+                NormalizedEmail = "USER@DOMAIN.COM",
+                AccessFailedCount = 0,
+                EmailConfirmed = true,
+                Id = "1",
+                LockoutEnabled = true,
+                UserName = "user",
+                NormalizedUserName = "USER",
+                PhoneNumber = "123456789",
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                TwoFactorEnabled = false
+            };
+
+            var pwdHasher = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = pwdHasher.HashPassword(user, "password");
+
+            return user;
+        }
+
+        public static List<ApplicationUser> CreateValidListOfUsers()
+        {
+            return new List<ApplicationUser>
+            {
+                CreateValidUser(),
+                new()
+                {
+                    ConcurrencyStamp = "6587454",
+                    Email = "new_user@altdomain.com",
+                    NormalizedEmail = "NEW_USER@ALTDOMAIN.COM",
+                    AccessFailedCount = 0,
+                    EmailConfirmed = true,
+                    Id = "2",
+                    LockoutEnabled = true,
+                    UserName = "new_user",
+                    NormalizedUserName = "NEW_USER",
+                    PhoneNumber = "665665665",
+                    PhoneNumberConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    TwoFactorEnabled = false
+                }
+            };
+        }
+    }
+}
