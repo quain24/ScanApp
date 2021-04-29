ef485b2881fd12dc8be5853b6bb69fa62e4bccde
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-04-29 14:19:39 +0200

    Implement Options in ScanAppTable to actually work.
    Disable grouping in SpareParts.razor

--- ScanApp/Components/Common/ScanAppTable/Options/ScanAppTableOptions.cs
+++ ScanApp/Components/Common/ScanAppTable/Options/ScanAppTableOptions.cs
@@ -5,6 +5,5 @@
         public bool ShowToolBar { get; set; } = true;
         public bool AllowGrouping { get; set; } = true;
         public bool AllowFiltering { get; set; } = true;
-        public bool AllowMultiSelection { get; set; } = true;
     }
 }
