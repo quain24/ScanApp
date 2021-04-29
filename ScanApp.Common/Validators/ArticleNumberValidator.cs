f0068ef8083a3db8b6598b174c0efdd45595c2ae
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-04-29 10:51:42 +0200

    Issue #10 #4 #9 GitLab - fix multiple issues involving validation, submit button
    Add ArticleNumberValidator.cs for #4
    
    Add IsSubmitButtonDisabled method to solve #9
    
    Add simple check in Submit method to solve #10

---
+++ ScanApp.Common/Validators/ArticleNumberValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+
+namespace ScanApp.Common.Validators
+{
+    public class ArticleNumberValidator<T, TProperty> : PropertyValidator<T, TProperty>
+    {
+        public override string Name => "Article number validator";
+
+        public override bool IsValid(ValidationContext<T> context, TProperty value)
+        {
+            if (value is not string articleNumber)
+            {
+                return false;
+            }
+
+            if (articleNumber.StartsWith(' ') || articleNumber.EndsWith(' '))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < articleNumber.Length; i++)
+            {
+                if (i == articleNumber.Length - 1)
+                {
+                    break;
+                }
+
+                if (articleNumber[i].Equals(' ') && articleNumber[i+1].Equals(' '))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected override string GetDefaultMessageTemplate(string errorCode)
+        {
+            return "\"{PropertyValue}\" is not a valid article number.";
+        }
+    }
+}
