2d6803e6129d8d21843e8bae16d055f6c2bb757d
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-05-06 11:28:20 +0200

    Add ValidationBehavior documentation

--- ScanApp.Application/Common/Behaviors/ValidationBehaviour.cs
+++ ScanApp.Application/Common/Behaviors/ValidationBehaviour.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using ScanApp.Application.Common.Helpers.Result;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -11,8 +12,11 @@ using System.Threading.Tasks;
 namespace ScanApp.Application.Common.Behaviors
 {
     /// <summary>
-    /// Will validate every request that provides a <see cref="Result"/> or <see cref="Result{T}"/> using corresponding validators
+    /// Validate every <typeparamref name="TRequest"/> passing through it using validators of type <see cref="AbstractValidator{TRequest}"/> assigned to said <typeparamref name="TRequest"/>
     /// </summary>
+    /// <remarks>This class handlers validation only if <typeparamref name="TResponse"/> parameter is of type <see cref="Result"/> or <see cref="Result{T}"/> or derived</remarks>
+    /// <typeparam name="TRequest">Type of handled request</typeparam>
+    /// <typeparam name="TResponse">Type of response that will be returned by this behavior</typeparam>
     public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
         where TRequest : IRequest<TResponse>
         where TResponse : Result, new()
@@ -21,16 +25,37 @@ namespace ScanApp.Application.Common.Behaviors
         private readonly ILogger<ValidationBehaviour<TRequest, TResponse>> _logger;
         private readonly IHttpContextAccessor _accessor;
 
+        /// <summary>
+        /// Creates new instance of <see cref="ValidationBehaviour{TRequest,TResponse}"/>
+        /// </summary>
+        /// <param name="validators">Collection of <see cref="IValidator{TRequest}"/> assigned to particular <typeparamref name="TRequest"/></param>
+        /// <param name="logger">Logger instance that will be used to log passing requests data</param>
+        /// <param name="accessor">Accessor that will provide user data, such as name</param>
+        /// <exception cref="ArgumentNullException">When <paramref name="logger"/> is <see langword="null"/></exception>
         public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators, ILogger<ValidationBehaviour<TRequest, TResponse>> logger, IHttpContextAccessor accessor)
         {
             _validators = validators;
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _accessor = accessor;
         }
 
+        /// <summary>
+        /// Handles <typeparamref name="TRequest"/> passing through mediator pipeline - validates those request by using <see cref="IValidator{T}"/> collection of validators that were assigned to given <paramref name="request"/><br/>
+        /// If <paramref name="request"/> was invalid, information will be logged - including all validation errors and name of user that run that request, if possible.
+        /// </summary>
+        /// <remarks><paramref name="cancellationToken"/> is not used in this implementation of <see cref="IPipelineBehavior{TRequest,TResponse}"/><br/>
+        /// This <see cref="IPipelineBehavior{TRequest,TResponse}"/> is final - if validation fails, new <typeparamref name="TResponse"/> is returned and pipeline is stopped
+        /// </remarks>
+        /// <param name="request">Incoming request</param>
+        /// <param name="cancellationToken">(not used) A token that can be used to request cancellation of the asynchronous operation</param>
+        /// <param name="next">Awaitable delegate for the next action in the pipeline. Eventually this delegate represents the handler.</param>
+        /// <returns>
+        /// <para>Awaitable task returning the <typeparamref name="TResponse"/> if <paramref name="request"/> was valid</para>
+        /// <para>Awaitable task returning new <typeparamref name="TResponse"/> containing error code and combined list of validation errors if <paramref name="request"/> was invalid</para>
+        /// </returns>
         public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            if (!_validators.Any())
+            if (!_validators?.Any() ?? true)
                 return next();
 
             var failures = _validators
