f02761387b80a4105f2f074148663577febd48e4
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-05-13 15:47:26 +0200

    Add ColumnConfig that replaces Header class
    Class enables easy column configuration for
    ScanAppTable component

---
+++ ScanApp.Tests/UnitTests/BlazorServerGui/Components/Common/ScanAppTable/Options/ColumnConfigTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using ScanApp.Components.Common.ScanAppTable.Options;
+using System;
+using System.Collections.Generic;
+using ScanApp.Common.Extensions;
+using Xunit;
+
+namespace ScanApp.Tests.UnitTests.BlazorServerGui.Components.Common.ScanAppTable.Options
+{
+    public class ColumnConfigTests
+    {
+        public class MyClass
+        {
+            public string A { get; }
+            public int B { get; }
+
+            public MyClass(string a, int b)
+            {
+                A = a;
+                B = b;
+            }
+        }
+
+        [Fact]
+        public void Will_create_instance()
+        {
+            var subject = new ColumnConfig<MyClass>(s => s.A, "name");
+
+            subject.Should().BeOfType<ColumnConfig<MyClass>>();
+        }
+
+        [Fact]
+        public void Will_create_instance_when_given_built_in_type()
+        {
+            var subject = new ColumnConfig<string>(s => s, "string");
+
+            subject.Should().BeOfType<ColumnConfig<string>>();
+        }
+
+        [Fact]
+        public void Will_create_instance_if_not_given_display_name()
+        {
+            var subject = new ColumnConfig<MyClass>(s => s.A, null);
+
+            subject.Should().BeOfType<ColumnConfig<MyClass>>();
+        }
+
+        [Fact]
+        public void Will_create_instance_with_optional_parameters()
+        {
+            var subject = new ColumnConfig<MyClass>(s => s.A, "A", isEditable: false, isGroupable: false);
+
+            subject.Should().BeOfType<ColumnConfig<MyClass>>();
+            Assert.False(subject.IsEditable);
+            Assert.False(subject.IsGroupable);
+            Assert.True(subject.IsFilterable);
+            Assert.True(subject.IsSelectable);
+        }
+
+        [Fact]
+        public void Will_throw_arg_null_exc_if_not_given_name_selector()
+        {
+            Action act = () => _ = new ColumnConfig<MyClass>(null, "name");
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Will_throw_arg_exc_if_display_name_contains_only_whitespace()
+        {
+            Action act = () => _ = new ColumnConfig<MyClass>(s => s.A, "   ");
+
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ ScanApp/Components/Common/ScanAppTable/Options/ColumnConfig.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.Extensions.Logging;
+using ScanApp.Common;
+using ScanApp.Models.SpareParts;
+
+namespace ScanApp.Components.Common.ScanAppTable.Options
+{
+    public class ColumnConfig<T>
+    {
+        public Expression<Func<T, object>> _columnNameSelector { get; init; }
+        public string PropertyName { get; }
+        public string DisplayName { get;}
+        public bool IsFilterable { get; private init; }
+        public bool IsEditable { get; private init; }
+        public bool IsSelectable { get; private init; }
+        public bool IsGroupable { get; private init; }
+        private IValidator cc;
+
+        public ColumnConfig(Expression<Func<T, object>> columnNameSelector, string displayName, bool isFilterable = true,
+            bool isEditable = true, bool isSelectable = true, bool isGroupable = true)
+        {
+            _columnNameSelector = columnNameSelector ?? throw new ArgumentNullException(nameof(columnNameSelector));
+
+            PropertyName = ExtractPropertyName();
+            DisplayName = displayName switch
+            {
+                null => PropertyName,
+                var s when string.IsNullOrWhiteSpace(s) => throw new ArgumentException("Display name cannot contain only whitespaces.", nameof(displayName)),
+                _ => displayName
+            };
+
+            IsFilterable = isFilterable;
+            IsEditable = isEditable;
+            IsSelectable = isSelectable;
+            IsGroupable = isGroupable;
+        }
+
+        private string ExtractPropertyName()
+        {
+            return _columnNameSelector.Body switch
+            {
+                MemberExpression m => m.Member.Name,
+                UnaryExpression { Operand: MemberExpression m } => m.Member.Name,
+                { } m => m.Type.Name
+            };
+        }
+
+    }
+}
