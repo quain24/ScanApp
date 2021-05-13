494bf3c4142075f84d2a9d540adfcfa0cdab54b5
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-05-13 15:48:15 +0200

    Add ColumnValidator class
    The class enables configuring validators for fields in
    editing and filtering dialog

---
+++ ScanApp/Components/Common/ScanAppTable/Options/ColumnValidator.cs
@@ -0,0 +1,30 @@
+﻿using ScanApp.Common;
+using System;
+
+namespace ScanApp.Components.Common.ScanAppTable.Options
+{
+    public class ColumnValidator<T>
+    {
+        private Type _type { get; set; }
+        public string PropertyName { get; init; }
+        public FluentValidationWrapper<T> Validator { get; init; }
+
+        public ColumnValidator(string propertyName, FluentValidationWrapper<T> validator)
+        {
+            _type = typeof(T);
+            if (Nullable.GetUnderlyingType(_type) == null && _type != typeof(string))
+            {
+                throw new ArgumentException("ColumnValidator class can only use nullable types or a string type.");
+            }
+
+            Validator = validator ?? throw new ArgumentNullException(nameof(validator));
+
+            PropertyName = propertyName switch
+            {
+                null => throw new ArgumentNullException(nameof(propertyName)),
+                var s when string.IsNullOrWhiteSpace(s) => throw new ArgumentException("PropertyName cannot contain only whitespaces.", nameof(propertyName)),
+                _ => propertyName
+            };
+        }
+    }
+}
