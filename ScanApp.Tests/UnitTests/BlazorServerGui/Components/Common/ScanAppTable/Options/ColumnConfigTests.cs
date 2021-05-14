b9f9ffc5749bca713f08608691a9fca6ecaeb7dd
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-05-14 19:07:34 +0200

    Refactor ColumnConfig
    This version stands on dynamic and reflection - temporary,
    hopefully not permanent solution.

--- ScanApp.Tests/UnitTests/BlazorServerGui/Components/Common/ScanAppTable/Options/ColumnConfigTests.cs
+++ ScanApp.Tests/UnitTests/BlazorServerGui/Components/Common/ScanAppTable/Options/ColumnConfigTests.cs
@@ -1,71 +1,117 @@
 ﻿using FluentAssertions;
-using FluentValidation;
 using ScanApp.Components.Common.ScanAppTable.Options;
 using System;
-using System.Collections.Generic;
-using ScanApp.Common.Extensions;
+using FluentValidation;
+using ScanApp.Common;
+using ScanApp.Common.Validators;
+using ScanApp.Models.SpareParts;
 using Xunit;
 
 namespace ScanApp.Tests.UnitTests.BlazorServerGui.Components.Common.ScanAppTable.Options
 {
     public class ColumnConfigTests
     {
-        public class MyClass
-        {
-            public string A { get; }
-            public int B { get; }
-
-            public MyClass(string a, int b)
-            {
-                A = a;
-                B = b;
-            }
-        }
-
         [Fact]
         public void Will_create_instance()
         {
-            var subject = new ColumnConfig<MyClass>(s => s.A, "name");
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.B, "name");
 
-            subject.Should().BeOfType<ColumnConfig<MyClass>>();
+            subject.Should().BeOfType<ColumnConfig<OptionsFixtures>>();
         }
 
-        [Fact]
-        public void Will_create_instance_when_given_built_in_type()
-        {
-            var subject = new ColumnConfig<string>(s => s, "string");
-
-            subject.Should().BeOfType<ColumnConfig<string>>();
-        }
 
         [Fact]
         public void Will_create_instance_if_not_given_display_name()
         {
-            var subject = new ColumnConfig<MyClass>(s => s.A, null);
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.B, null);
 
-            subject.Should().BeOfType<ColumnConfig<MyClass>>();
+            subject.Should().BeOfType<ColumnConfig<OptionsFixtures>>();
+        }
+
+        [Fact]
+        public void Will_create_instance_when_given_value_type()
+        {
+            var subject = new ColumnConfig<int>(s => s, "int");
+
+            subject.Should().BeOfType<ColumnConfig<int>>();
+        }
+
+        [Fact]
+        public void  Will_have_correct_property_type_when_name_selector_value_type()
+        {
+            var subject = new ColumnConfig<double>(s => s, "str");
+
+            subject.PropertyType.Should().Be(typeof(double));
+        }
+
+        [Fact]
+        public void  Will_have_correct_property_type_when_name_selector_chooses_model_property()
+        {
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.A, null);
+
+            subject.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void  Will_have_correct_property_name_when_name_selector_chooses_model_property()
+        {
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.A, null);
+
+            subject.PropertyName.Should().Be(nameof(OptionsFixtures.A));
+        }
+
+        [Fact]
+        public void Will_have_correct_property_type_when_name_selector_chooses_sub_level_property()
+        {
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.D.Name, null);
+
+            subject.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void Will_have_correct_property_name_when_name_selector_chooses_sub_level_property()
+        {
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.D.Name, null);
+
+            subject.PropertyName.Should().Be(nameof(OptionsFixtures.D.Name));
+        }
+
+        [Fact]
+        public void Will_have_correct_property_type_when_name_selector_chooses_type()
+        {
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.D, null);
+
+            subject.PropertyType.Should().Be(typeof(SparePartGUIModel));
+        }
+
+        [Fact]
+        public void Will_have_correct_property_name_when_name_selector_chooses_type()
+        {
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.D, null);
+
+            subject.PropertyName.Should().Be(nameof(OptionsFixtures.D));
         }
 
         [Fact]
         public void Will_create_instance_with_optional_parameters()
         {
-            var subject = new ColumnConfig<MyClass>(s => s.A, "A")
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.A, "A")
             {
                 IsEditable = false,
                 IsGroupable = false
             };
 
-            subject.Should().BeOfType<ColumnConfig<MyClass>>();
-            Assert.False(subject.IsEditable);
-            Assert.False(subject.IsGroupable);
-            Assert.True(subject.IsFilterable);
-            Assert.True(subject.IsSelectable);
+            subject.Should().BeOfType<ColumnConfig<OptionsFixtures>>();
+            subject.IsEditable.Should().BeFalse();
+            subject.IsGroupable.Should().BeFalse();
+            subject.IsFilterable.Should().BeTrue();
+            subject.IsSelectable.Should().BeTrue();
         }
 
         [Fact]
         public void Will_throw_arg_null_exc_if_not_given_name_selector()
         {
-            Action act = () => _ = new ColumnConfig<MyClass>(null, "name");
+            Action act = () => _ = new ColumnConfig<OptionsFixtures>(null, "name");
 
             act.Should().Throw<ArgumentNullException>();
         }
@@ -73,9 +119,27 @@ namespace ScanApp.Tests.UnitTests.BlazorServerGui.Components.Common.ScanAppTable
         [Fact]
         public void Will_throw_arg_exc_if_display_name_contains_only_whitespace()
         {
-            Action act = () => _ = new ColumnConfig<MyClass>(s => s.A, "   ");
+            Action act = () => _ = new ColumnConfig<OptionsFixtures>(s => s.A, "   ");
 
             act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Will_create_instance_with_proper_validator()
+        {
+            var validator = new FluentValidationWrapper<string>(x => x.Cascade(CascadeMode.Stop).Length(10));
+            var val = new IdentityNamingValidator<string, string>();
+            var subject = new ColumnConfig<OptionsFixtures>(s => s.A, "A", new Vall());
+            var a = subject.Validator("aaa");
+            var x = "";
+        }
+    }
+
+    public class Vall : AbstractValidator<string>
+    {
+        public Vall()
+        {
+            RuleFor(c => c).Length(10);
         }
     }
 }
--- ScanApp/Components/Common/ScanAppTable/Options/ColumnConfig.cs
+++ ScanApp/Components/Common/ScanAppTable/Options/ColumnConfig.cs
@@ -1,46 +1,117 @@
 ﻿using FluentValidation;
+using FluentValidation.Validators;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using Microsoft.CodeAnalysis.Operations;
-using Microsoft.Extensions.Logging;
-using ScanApp.Common;
-using ScanApp.Models.SpareParts;
 
 namespace ScanApp.Components.Common.ScanAppTable.Options
 {
     public class ColumnConfig<T>
     {
-        public Expression<Func<T, object>> _columnNameSelector { get; init; }
+        private readonly string _errorMessage;
+        private readonly Expression<Func<T, object>> _columnNameSelector;
+
+        public Func<dynamic, IEnumerable<string>> Validator { get; }
         public string PropertyName { get; }
-        public string DisplayName { get;}
+        public string DisplayName { get; }
+        public Type PropertyType { get; }
         public bool IsFilterable { get; init; } = true;
         public bool IsEditable { get; init; } = true;
         public bool IsSelectable { get; init; } = true;
         public bool IsGroupable { get; init; } = true;
+
+        public ColumnConfig(Expression<Func<T, object>> columnNameSelector, string displayName, IPropertyValidator validator, string errorMessage) : this(columnNameSelector, displayName)
+        {
+            _errorMessage = errorMessage;
+            Validator = CreateStrongTypeValidatorFrom(validator);
+        }
+
+        public ColumnConfig(Expression<Func<T, object>> columnNameSelector, string displayName, IValidator validator) : this(columnNameSelector, displayName)
+        {
+            Validator = validator is not null ? CreateStrongTypeValidatorFrom(validator) : null;
+        }
 
         public ColumnConfig(Expression<Func<T, object>> columnNameSelector, string displayName)
         {
             _columnNameSelector = columnNameSelector ?? throw new ArgumentNullException(nameof(columnNameSelector));
 
             PropertyName = ExtractPropertyName();
-            DisplayName = displayName switch
-            {
-                null => PropertyName,
-                var s when string.IsNullOrWhiteSpace(s) => throw new ArgumentException("Display name cannot contain only whitespaces.", nameof(displayName)),
-                _ => displayName
-            };
+            DisplayName = SetDisplayName(displayName);
+
+            PropertyType = ExtractValidatedType();
         }
 
         private string ExtractPropertyName()
         {
             return _columnNameSelector.Body switch
             {
-                MemberExpression m => m.Member.Name,
+                UnaryExpression { NodeType: ExpressionType.Convert or ExpressionType.ConvertChecked } unary => unary.Operand.Type.Name,
                 UnaryExpression { Operand: MemberExpression m } => m.Member.Name,
+                MemberExpression m => m.Member.Name,
                 { } m => m.Type.Name
             };
         }
 
+        private string SetDisplayName(string name)
+        {
+            return name switch
+            {
+                null => PropertyName,
+                var s when string.IsNullOrWhiteSpace(s) => throw new ArgumentException("Display name cannot contain only whitespaces.", nameof(name)),
+                _ => name
+            };
+        }
+
+        private Type ExtractValidatedType()
+        {
+            return _columnNameSelector.Body switch
+            {
+                { NodeType: ExpressionType.Convert or ExpressionType.ConvertChecked } and UnaryExpression unary => unary.Operand.Type,
+                { NodeType: ExpressionType.Convert or ExpressionType.ConvertChecked } and MemberExpression member => member.Type,
+                _ => _columnNameSelector.Body.Type
+            };
+        }
+
+        private Func<dynamic, IEnumerable<string>> CreateStrongTypeValidatorFrom(IValidator validator)
+        {
+            var emptyContextType = typeof(ValidationContext<>);
+            var contextType = emptyContextType.MakeGenericType(PropertyType);
+
+            return value =>
+            {
+                var context = Activator.CreateInstance(contextType, value);
+                var res = validator.Validate(context);
+
+                if (res.IsValid)
+                    return Array.Empty<string>();
+
+                var errors = new List<string>(res.Errors.Count);
+                foreach (var failure in res.Errors)
+                {
+                    errors.Add(failure.ErrorMessage);
+                }
+
+                return errors;
+            };
+        }
+
+        private Func<dynamic, IEnumerable<string>> CreateStrongTypeValidatorFrom(IPropertyValidator validator)
+        {
+            var emptyContextType = typeof(PropertyValidator<,>);
+            var contextType = emptyContextType.MakeGenericType(PropertyType, PropertyType);
+
+            return value =>
+            {
+                dynamic vali = Convert.ChangeType(validator, validator.GetType());
+                var res = vali.IsValid(null, value);
+
+                if (res is true)
+                    return Array.Empty<string>();
+                return new List<string>(1)
+                {
+                    _errorMessage
+                };
+            };
+        }
     }
 }
