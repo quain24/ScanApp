9888e80adac48ad57c3b12f9d5cf5a9e7c960b85
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-05-14 19:06:13 +0200

    Validator extensions now works on interface instead of abstract class.
    This should simplify using them on injected derived classes.

--- ScanApp/Common/Extensions/ValidatorExtensions.cs
+++ ScanApp/Common/Extensions/ValidatorExtensions.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 namespace ScanApp.Common.Extensions
 {
     /// <summary>
-    /// Provides extension methods for <see cref="AbstractValidator{T}"/> to simplify using such objects with<br/>
-    /// <see cref="MudBlazor"/> components such as <see cref="MudTextField{T}"/> and others that support validation<br/>
+    /// Provides extension methods for <see cref="IValidator{T}"/> to simplify using such objects with<br/>
+    /// <see cref="MudBlazor"/> components such as <see cref="MudTextField{T}"/> and others that support validation<br/><br/>
     /// Delegates created by those extensions will threat <see langword="null"/> input as invalid!
     /// </summary>
-    public static class AbstractValidatorExtensions
+    public static class ValidatorExtensions
     {
         /// <summary>
         /// Creates <strong>ASYNC</strong> delegate to be used with <see cref="MudBlazor"/> components that supports validation
@@ -21,36 +21,34 @@ namespace ScanApp.Common.Extensions
         /// <param name="validator">Source validator from which a validating delegate will be created</param>
         /// <returns>Delegate that can be assigned to <see cref="MudBlazor"/> filed supporting validation</returns>
         /// <exception cref="ArgumentNullException">Given <paramref name="validator"/> is <see langword="null"/></exception>
-        public static Func<T, Task<IEnumerable<string>>> ToAsyncMudFormFieldValidator<T>(this AbstractValidator<T> validator)
+        public static Func<T, Task<IEnumerable<string>>> ToAsyncMudFormFieldValidator<T>(this IValidator<T> validator)
         {
             _ = validator ?? throw new ArgumentNullException(nameof(validator));
             return async value =>
             {
-                var res = await validator.ValidateAsync(value);
-                if (res.IsValid)
-                    return Array.Empty<string>();
-
-                return ExtractErrorsFrom(res);
+                var res = await validator.ValidateAsync(value).ConfigureAwait(false);
+                return res.IsValid
+                    ? Array.Empty<string>()
+                    : ExtractErrorsFrom(res);
             };
         }
 
         /// <summary>
-        /// Creates delegate to be used with <see cref="MudBlazor"/> components that supports validation
+        /// Creates delegate to be used with <see cref="MudBlazor"/> components that supports validation.
         /// </summary>
         /// <typeparam name="T">Input type of field that will be validated</typeparam>
         /// <param name="validator">Source validator from which a validating delegate will be created</param>
         /// <returns>Delegate that can be assigned to <see cref="MudBlazor"/> filed supporting validation</returns>
         /// <exception cref="ArgumentNullException">Given <paramref name="validator"/> is <see langword="null"/></exception>
-        public static Func<T, IEnumerable<string>> ToMudFormFieldValidator<T>(this AbstractValidator<T> validator)
+        public static Func<T, IEnumerable<string>> ToMudFormFieldValidator<T>(this IValidator<T> validator)
         {
             _ = validator ?? throw new ArgumentNullException(nameof(validator));
             return value =>
             {
-                var res = validator.Validate(value);
-                if (res.IsValid)
-                    return Array.Empty<string>();
-
-                return ExtractErrorsFrom(res);
+                var result = validator.Validate(value);
+                return result.IsValid
+                    ? Array.Empty<string>()
+                    : ExtractErrorsFrom(result);
             };
         }
 
