fb2407a8e05035874a8b79711e0bb5f353a1f98f
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-05-17 14:41:22 +0200

    #28 - fix submit button enabled when article field is empty

---
+++ ScanApp.Common/Validators/PositiveNumbersOnlyValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace ScanApp.Common.Validators
+{
+    public class PositiveNumbersOnlyValidator : AbstractValidator<int?>
+    {
+        public PositiveNumbersOnlyValidator()
+        {
+            RuleFor(x => x)
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("Negative numbers are not allowed.");
+        }
+    }
+}
