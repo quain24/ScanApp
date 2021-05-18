ba98abe64e8c7d94dd37557fc6fe4d395427b14a
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-05-18 13:33:26 +0200

    #13 Vertical scrolling is enabled only under certain conditions
    Add DialogStyle class which returns styles for
    dialogs in ScanAppTable. The vertical scroll is now only
    enabled if there are more than 5 properties in the table.

---
+++ ScanApp/Components/Common/ScanAppTable/Options/DialogStyle.cs
@@ -0,0 +1,17 @@
+﻿namespace ScanApp.Components.Common.ScanAppTable.Options
+{
+    public static class DialogStyle
+    {
+        public static string GetDialogStyle(int length)
+        {
+            if (length >= 5)
+            {
+                return "max-height: 500px; overflow-y: scroll;";
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
