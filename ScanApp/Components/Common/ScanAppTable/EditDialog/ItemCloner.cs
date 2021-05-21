0d6eb4ec43b96f336df37be011cfec9eb0fc8671
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-05-21 14:33:40 +0200

    Add new way of deep cloning objects
    Dev can now deep clone object by using 'object'.copy()

--- ScanApp/Components/Common/ScanAppTable/EditDialog/ItemCloner.cs
+++ ScanApp/Components/Common/ScanAppTable/EditDialog/ItemCloner.cs
@@ -1,13 +1,27 @@
-﻿using System.Text.Json;
+﻿using Newtonsoft.Json;
 
-namespace ScanApp.Components.Common.ScanAppTable.EditDialog
+namespace ScanApp.Common.Extensions
 {
     public static class ItemCloner
     {
-        public static TItem Clone<TItem>(TItem item)
+        /// <summary>
+        /// Perform a deep Copy of the object, using Json as a serialization method. NOTE: Private members are not cloned using this method.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <returns>The copied object.</returns>
+        public static T CloneJson<T>(this T source)
         {
-            var serialized = JsonSerializer.Serialize(item);
-            return JsonSerializer.Deserialize<TItem>(serialized);
+            // Don't serialize a null object, simply return the default for that object
+            if (ReferenceEquals(source, null)) return default;
+
+            // initialize inner objects individually
+            // for example in default constructor some list property initialized with some values,
+            // but in 'source' these items are cleaned -
+            // without ObjectCreationHandling.Replace default constructor values will be added to result
+            var deserializeSettings = new JsonSerializerSettings { ObjectCreationHandling = ObjectCreationHandling.Replace };
+
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(source), deserializeSettings);
         }
     }
 }
