f15cf2ef86cbfb825cb2730c9efef340ab933b3d
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-05-21 12:55:26 +0200

    #19 - Change how ScanAppTableOptions operate.
    Now ShowToolBar is controlled automatically.
    If one option ie. AllowFiltering is enabled, ShowToolBar
    is enabled automatically as well. On the other hand,
    if both filtering and grouping is disabled, toolbar
    is disabled as well.
    
    Moreover, if no Options are passed to the ScanAppTable,
    all operations will be enabled by default.
    
    Using default constructor of ScanAppTableOptions
    without any parameters will result in all operations
    to be disabled.

--- ScanApp/Components/Common/ScanAppTable/Options/ScanAppTableOptions.cs
+++ ScanApp/Components/Common/ScanAppTable/Options/ScanAppTableOptions.cs
@@ -1,10 +1,48 @@
-﻿namespace ScanApp.Components.Common.ScanAppTable.Options
+﻿using System.Transactions;
+
+namespace ScanApp.Components.Common.ScanAppTable.Options
 {
     public class ScanAppTableOptions
     {
-        public bool ShowToolBar { get; set; } = true;
-        public bool AllowGrouping { get; set; } = true;
-        public bool AllowFiltering { get; set; } = true;
-        public bool AllowNegativeNumbersInFiltering { get; set; } = true;
+        private readonly bool _allowGrouping;
+        private readonly bool _allowFiltering;
+
+        public ScanAppTableOptions()
+        {
+            ShowToolBar = IsToolBarEnabled();
+        }
+
+        public bool ShowToolBar { get; private set; }
+
+        public bool AllowGrouping
+        {
+            get => _allowGrouping;
+            init
+            {
+                if (value is true) ShowToolBar = true;
+                _allowGrouping = value;
+            }
+        }
+
+        public bool AllowFiltering
+        {
+            get => _allowFiltering;
+            init
+            {
+                if (value is true) ShowToolBar = true;
+                _allowFiltering = value;
+            }
+        }
+
+        public bool AllowNegativeNumbersInFiltering { get; init; } = true;
+
+        private bool IsToolBarEnabled()
+        {
+            if (AllowGrouping is false && AllowFiltering is false)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
