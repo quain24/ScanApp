407e932c21727dedb7a315c5c8a4f0977ffb6e49
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-05-22 17:24:32 +0200

    Add a way to directly get underlying type from member info.
    Underlying type is available only from fields and properties.
    This is enough for current functionality.

---
+++ ScanApp/Services/MemberInfoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace ScanApp.Services
+{
+    public static class MemberInfoExtensions
+    {
+        /// <summary>
+        /// Extracts underlying <see cref="Type"/> from given <paramref name="info"/>.
+        /// </summary>
+        /// <param name="info">Object to have its underlying type extracted</param>
+        /// <returns>Type of object from which this <see cref="MemberInfo"/> was extracted.</returns>
+        /// <exception cref="ArgumentException">Given <paramref name="info"/> was not <see cref="FieldInfo"/> or <see cref="PropertyInfo"/>.</exception>
+        public static Type GetUnderlyingType(this MemberInfo info)
+        {
+            return info?.MemberType switch
+            {
+                MemberTypes.Field => ((FieldInfo)info).FieldType,
+                MemberTypes.Property => ((PropertyInfo)info).PropertyType,
+                _ => throw new ArgumentException("Underlying type is returned only for properties and fields.")
+            };
+        }
+    }
+}
