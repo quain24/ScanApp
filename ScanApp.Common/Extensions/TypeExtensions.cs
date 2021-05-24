fcd9c5e464581f46110de72d52a1daf622b66e9c
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-05-24 16:14:53 +0200

    Add time field generation in field constructor.
    Table now have separate formatting function for outputs.
    Added extension for getting default for given type from type variable.

---
+++ ScanApp.Common/Extensions/TypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ScanApp.Common.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static object GetDefaultValue(this Type t)
+        {
+            if (t.IsValueType && Nullable.GetUnderlyingType(t) == null)
+                return Activator.CreateInstance(t);
+            return null;
+        }
+    }
+}
