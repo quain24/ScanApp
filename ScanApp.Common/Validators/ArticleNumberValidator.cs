ce58f389b0af2ca735182fce06105c67f317ab97
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-05-24 14:32:48 +0200

    #29 - Split ArticleNumberValidator into two rules instead of one.
    Two rules needed to be constructed because one of
    them overrode the .NotEmpty() rule with its
    When condition. The .NotEmpty() rule was then
     ignored.

--- ScanApp.Common/Validators/ArticleNumberValidator.cs
+++ ScanApp.Common/Validators/ArticleNumberValidator.cs
@@ -12,10 +12,14 @@ namespace ScanApp.Common.Validators
                 .MinimumLength(5)
                 .WithMessage("Article number is too short. Minimum 5 digits required.")
                 .MaximumLength(20)
-                .WithMessage("Article number is too long. Maximum 20 digits required.")
+                .WithMessage("Article number is too long. Maximum 20 digits required.");
+
+
+            RuleFor(n => n)
                 .Must(n => !(n[0].Equals(' ') || n[^1].Equals(' ') || n.Contains("  ")))
                 .When(n => string.IsNullOrEmpty(n) is false)
                 .WithMessage("\"{PropertyValue}\" is not a valid article number.");
+
         }
     }
 }
