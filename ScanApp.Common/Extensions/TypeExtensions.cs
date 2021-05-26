4aaddc1ec493aaef95004807177b1ee83b1ef285
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-05-26 22:15:08 +0200

    Add IsNumeric and LineNumber helpers.
    Line number can be used when creating blazor
    components in render trees (instead of manual sequence number settings).
    Is numeric will check if given type is a numerical type.

--- ScanApp.Common/Extensions/TypeExtensions.cs
+++ ScanApp.Common/Extensions/TypeExtensions.cs
@@ -4,11 +4,17 @@ namespace ScanApp.Common.Extensions
 {
     public static class TypeExtensions
     {
-        public static object GetDefaultValue(this Type t)
+        public static dynamic GetDefaultValue(this Type t)
         {
             if (t.IsValueType && Nullable.GetUnderlyingType(t) == null)
                 return Activator.CreateInstance(t);
             return null;
+        }
+
+        public static bool IsNumeric(this Type value)
+        {
+            var t = Nullable.GetUnderlyingType(value) ?? value;
+            return (t.IsPrimitive && t != typeof(char)) || t == typeof(decimal);
         }
     }
 }
---
+++ ScanApp.Common/Helpers/LineNumber.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ScanApp.Common.Helpers
+{
+    public static class LineNumber
+    {
+        /// <summary>
+        /// Returns line number on which this method was called.
+        /// </summary>
+        /// <param name="line">Compile time resolved parameter - will not work if manually set.</param>
+        /// <returns>Line number.</returns>
+        public static int Get([CallerLineNumber] int line = 0) => line;
+    }
+}
