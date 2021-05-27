67c53887e0b0c1105aaca87b579c9d6033ff20e1
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-05-27 22:33:38 +0200

    Add InBetween filtering operation

---
+++ ScanApp/Components/Common/AltTableTest/IFilter.cs
@@ -0,0 +1,13 @@
+﻿using ScanApp.Components.Common.ScanAppTable.Options;
+using System;
+using System.Collections.Generic;
+
+namespace ScanApp.Components.Common.AltTableTest
+{
+    public interface IFilter<T>
+    {
+        ColumnConfig<T> ColumnConfig { get; }
+
+        IEnumerable<T> Run(IEnumerable<T> source);
+    }
+}
---
+++ ScanApp/Components/Common/AltTableTest/InBetweenInclusiveFilter.cs
@@ -0,0 +1,80 @@
+﻿using ScanApp.Common.Extensions;
+using ScanApp.Components.Common.ScanAppTable.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScanApp.Components.Common.AltTableTest
+{
+    public class InBetweenInclusiveFilter<T> : IFilter<T>
+    {
+        private readonly dynamic _from;
+        private readonly dynamic _to;
+
+        private readonly string _message = $"argument can be either a {nameof(DateTime)}, {nameof(DateTimeOffset)}, {nameof(TimeSpan)}, numeric or null.";
+
+        public InBetweenInclusiveFilter(ColumnConfig<T> columnConfig, dynamic from, dynamic to)
+        {
+            if (CanBeUsed(from) is false)
+                throw new ArgumentException($"'{nameof(from)}' {_message}", nameof(from));
+
+            if (CanBeUsed(to) is false)
+                throw new ArgumentException($"'{nameof(to)}' {_message}", nameof(to));
+
+            if (TypesMatch(from, to) is false)
+                throw new ArgumentException($"'{nameof(from)}' type and '{nameof(to)}' type are not matched.");
+
+            ColumnConfig = columnConfig;
+            _from = from;
+            _to = to;
+
+            if (CanBeUsed(columnConfig.PropertyType) is false)
+            {
+                throw new ArgumentException($"Type of property being filtered ({columnConfig.PropertyType.Name})" +
+                                            $" stored in {nameof(columnConfig)} is not compatible with {nameof(InBetweenInclusiveFilter<T>)} sorting algorithm.");
+            }
+
+            if (TypesMatch(from, columnConfig.PropertyType) is false || TypesMatch(to, columnConfig.PropertyType))
+            {
+                throw new ArgumentException($"Type of property being filtered ({columnConfig.PropertyType.Name})" +
+                                            $" is not the same as types of '{nameof(from)}' or/and '{nameof(to)}' parameters.");
+            }
+        }
+
+        private static bool CanBeUsed(dynamic value)
+        {
+            return value is DateTime or TimeSpan or DateTimeOffset ||
+                   (((Type)value?.GetType())?.IsNumeric() ?? true);
+        }
+
+        private static bool TypesMatch(dynamic one, dynamic two)
+        {
+            if (one is null || two is null) return true;
+            return (one is Type ? one : (Type)one.GetType()) == (two is Type ? two : (Type)two.GetType());
+        }
+
+        public ColumnConfig<T> ColumnConfig { get; }
+
+        public IEnumerable<T> Run(IEnumerable<T> source)
+        {
+            if (_from is null && _to is null)
+                return source.ToArray();
+
+            return source.Where(i =>
+            {
+                var value = ColumnConfig.GetValueFrom(i);
+                if (_from is not null && _to is not null)
+                {
+                    return value is not null && value >= _from && value <= _to;
+                }
+
+                if (_from is null)
+                {
+                    return value is null || value <= _to;
+                }
+
+                return value is null || value >= _from;
+            });
+        }
+    }
+}
