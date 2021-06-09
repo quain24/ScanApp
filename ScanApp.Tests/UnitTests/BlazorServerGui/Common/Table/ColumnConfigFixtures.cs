860c2ff31b84cf5952fd2717f27a86aa87f554bc
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-09 13:25:59 +0200

    Add initial tests for column config.

---
+++ ScanApp.Tests/UnitTests/BlazorServerGui/Common/Table/ColumnConfigFixtures.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace ScanApp.Tests.UnitTests.BlazorServerGui.Common.Table
+{
+    public static class ColumnConfigFixtures
+    {
+        public class TestObject
+        {
+            public string AString { get; set; }
+            public int AnInt { get; set; }
+
+            public int AnIntField;
+            public int? AnNullableInt { get; set; }
+
+            public SubClass SubClassProp { get; set; }
+            public SubClass SubClassField;
+            public SubClassPar SubClassParamField;
+            public TestStruct TestStructProp { get; set; }
+        }
+
+        public class SubClass
+        {
+            public string AString { get; set; }
+            public DateTime? NullableDateTime { get; set; }
+
+            public double DoubleField;
+            public int? NullableIntField;
+
+            public SubClass SubClassPropInSubClass { get; set; }
+            public SubClassPar SubClassParFieldInSubClass;
+            public TestStruct SubClassStructFieldInSubClass;
+        }
+
+        public class SubClassPar
+        {
+
+            public SubClassPar(string par)
+            {
+                AString = par;
+            }
+            public string AString { get; set; }
+            public DateTime? NullableDateTime { get; set; }
+
+            public double DoubleField;
+            public int? NullableIntField;
+
+            public SubClass SubClassPropInSubParamClass { get; set; }
+            public SubClass SubClassFieldInSubParamClass;
+        }
+
+        public struct TestStruct
+        {
+            public int IntVal;
+
+            public string StrVal;
+
+            public DateTime? DateTimeNullableVal;
+
+            public SubStruct StructVal;
+            public SubClass  ClassVal;
+        }
+
+        public struct SubStruct
+        {
+            public int IntVal;
+
+            public string StrVal;
+
+            public DateTime? DateTimeNullableVal;
+        }
+
+        public class ColumnConfigExtensionsTheoryData : TheoryData<TestObject, Expression<Func<TestObject, object>>, dynamic, TestObject>
+        {
+            public ColumnConfigExtensionsTheoryData()
+            {
+                Add(new TestObject(), c => c.SubClassField, new SubClass(){AString = "wow"}, new TestObject {SubClassField = new SubClass(){AString = "wow"}});
+                Add(new TestObject(), c => c.TestStructProp, new TestStruct(){StrVal = "wow"}, new TestObject {TestStructProp = new TestStruct(){StrVal = "wow"}});
+                Add(new TestObject(), c => c, new TestObject(){AString = "wowww"}, new TestObject { AString = "wowww" });
+                Add(new TestObject(), c => c.AString, "test value", new TestObject { AString = "test value" });
+                Add(new TestObject() {AnNullableInt = 10}, c => c.AnNullableInt, null, new TestObject());
+                Add(new TestObject(), c => c.AnNullableInt, null, new TestObject());
+                Add(new TestObject(), c => c.AnNullableInt, 10, new TestObject() { AnNullableInt = 10 });
+                Add(new TestObject(), c => c.AnInt, 1, new TestObject() { AnInt = 1 });
+                Add(new TestObject(), c => c.SubClassProp, new SubClass { AString = "string" }, new TestObject() { SubClassProp = new SubClass() { AString = "string" } });
+                Add(new TestObject(), c => c.SubClassField, new SubClass { AString = "string" }, new TestObject() { SubClassField = new SubClass() { AString = "string" } });
+                Add(new TestObject { SubClassProp = new SubClass() }, c => c.SubClassProp.DoubleField, 10.5, new TestObject() { SubClassProp = new SubClass() { DoubleField = 10.5 } });
+                Add(new TestObject { SubClassParamField = new SubClassPar("aaa") }, c => c.SubClassParamField.AString, "value", new TestObject() { SubClassParamField = new SubClassPar("value") });
+                Add(new TestObject {SubClassParamField = new SubClassPar("aaa"){SubClassPropInSubParamClass = new SubClass(){SubClassParFieldInSubClass = new SubClassPar("aa"){AString = "old value"}}}},
+                    c => c.SubClassParamField.SubClassPropInSubParamClass.SubClassParFieldInSubClass.AString,
+                    "value",
+                    new TestObject {SubClassParamField = new SubClassPar("aaa"){SubClassPropInSubParamClass = new SubClass(){SubClassParFieldInSubClass = new SubClassPar("aa"){AString = "value"}}}});
+            }
+        }
+
+        public class PrimitiveTheoryDataInt : TheoryData<int, Expression<Func<int, object>>, int, int>
+        {
+            public PrimitiveTheoryDataInt()
+            {
+                Add(0, c => c, 999, 999);
+                Add(10, c => c, 999, 999);
+                Add(-10, c => c, 999, 999);
+            }
+        }
+
+        public class StructTheoryData : TheoryData<TestStruct, Expression<Func<TestStruct, object>>, dynamic, TestStruct>
+        {
+            public StructTheoryData()
+            {
+                Add(new TestStruct(){IntVal = 10}, c => c, new TestStruct(){IntVal = 20}, new TestStruct(){IntVal = 20});
+                Add(new TestStruct() { ClassVal = new SubClass() { AString = "initial" } },
+                    c => c.ClassVal.AString,
+                    "new str",
+                    new TestStruct() { ClassVal = new SubClass() { AString = "new str" } });
+                Add(new TestStruct() { ClassVal = new SubClass() { AString = "initial", SubClassStructFieldInSubClass = new TestStruct(){ClassVal = new SubClass(){AString = "nested old"}}}},
+                    c => c.ClassVal.SubClassStructFieldInSubClass.ClassVal.AString,
+                    "nested",
+                    new TestStruct() { ClassVal = new SubClass() { AString = "initial", SubClassStructFieldInSubClass = new TestStruct(){ClassVal = new SubClass(){AString = "nested"}}}});
+            }
+        }
+        public class InvalidStructTheoryData : TheoryData<TestStruct, Expression<Func<TestStruct, object>>, dynamic, TestStruct>
+        {
+            public InvalidStructTheoryData()
+            {
+                Add(new TestStruct(){IntVal = 0}, c => c.IntVal, 999, new TestStruct(){IntVal = 999});
+                Add(new TestStruct(){StructVal = new SubStruct(){IntVal = 1}, IntVal = 12}, c => c.StructVal.IntVal, 10, new TestStruct(){StructVal = new SubStruct(){IntVal = 10}, IntVal = 12});
+            }
+        }
+
+    }
+}
---
+++ ScanApp.Tests/UnitTests/BlazorServerGui/Common/Table/ColumnConfigTests.cs
@@ -0,0 +1,117 @@
+﻿using FluentAssertions;
+using Moq;
+using ScanApp.Common.Extensions;
+using ScanApp.Components.Common.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using Xunit;
+using static ScanApp.Tests.UnitTests.BlazorServerGui.Common.Table.ColumnConfigFixtures;
+
+namespace ScanApp.Tests.UnitTests.BlazorServerGui.Common.Table
+{
+    public class ColumnConfigTests
+    {
+        [Fact]
+        public void SetValue_throws_arg_null_exc_when_target_is_null()
+        {
+            var config = new ColumnConfig<ColumnConfigFixtures.TestObject>(target => target.AnInt);
+            Action act = () => config.SetValue(null, 1);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void SetValue_throws_arg_exc_when_target_has_different_type_than_value()
+        {
+            var target = new TestObject();
+            var config = new ColumnConfig<TestObject>(t => t.AnInt);
+            Action act = () => config.SetValue(target, "a");
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        [ClassData(typeof(ColumnConfigExtensionsTheoryData))]
+        public void SetValue_sets_value(TestObject initialTargetState, Expression<Func<TestObject, object>> expr, dynamic value, TestObject expected)
+        {
+            var target = initialTargetState;
+
+            var config = new ColumnConfig<TestObject>(expr);
+            TestObject result = config.SetValue(target, value);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [ClassData(typeof(PrimitiveTheoryDataInt))]
+        public void SetValue_sets_value_in_primitive(int initialTargetState, Expression<Func<int, object>> expr, int value, int expected)
+        {
+            var target = initialTargetState;
+            var config = new ColumnConfig<int>(expr);
+            var result = config.SetValue(target, value);
+
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SetValue_throws_arg_exc_when_trying_to_replace_value_in_struct()
+        {
+            var target = new TestStruct();
+            var sut = new ColumnConfig<TestStruct>(t => t.IntVal);
+
+            Action act = () => _ = sut.SetValue(target, 10);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void SetValue_throws_arg_exc_when_trying_to_replace_value_in_struct_nested_inside_struct()
+        {
+            var target = new TestStruct() { StructVal = new SubStruct() { IntVal = 1 }, IntVal = 12 };
+            var sut = new ColumnConfig<TestStruct>(t => t.StructVal.IntVal);
+
+            Action act = () => _ = sut.SetValue(target, 10);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void SetValue_throws_arg_exc_when_trying_to_replace_reference_type_in_struct()
+        {
+            var target = new TestStruct() { ClassVal = new SubClass() { AString = "test" } };
+            var sut = new ColumnConfig<TestStruct>(t => t.ClassVal);
+
+            Action act = () => _ = sut.SetValue(target, new SubClass() { AString = "wont happen" });
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        [ClassData(typeof(StructTheoryData))]
+        public void SetValue_sets_value_in_struct(TestStruct initialTargetState, Expression<Func<TestStruct, object>> expr, dynamic value, TestStruct expected)
+        {
+            var target = initialTargetState;
+            var config = new ColumnConfig<TestStruct>(expr);
+            TestStruct result = config.SetValue(target, value);
+
+            result.Should().BeEquivalentTo(expected, o =>
+            {
+                o.ComparingByMembers<TestStruct>();
+                return o;
+            });
+        }
+
+        [Theory]
+        [ClassData(typeof(InvalidStructTheoryData))]
+        public void SetValue_throws_when_modifying_insides_of_struct_that_are_not_reference_type(TestStruct initialTargetState, Expression<Func<TestStruct, object>> expr, dynamic value, TestStruct expected)
+        {
+            var target = initialTargetState;
+            var config = new ColumnConfig<TestStruct>(expr);
+            Action act = () => _ = config.SetValue(target, value);
+
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
