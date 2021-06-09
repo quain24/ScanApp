7bae1ac948f138443bc73b5e4e6ddba8c7556ce6
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-09 10:25:35 +0200

    Removed parallelization from filtering - gave unsorted result.
    Any performance bump from parallel work would be diminished by need to sort - given reasonable amount of data.
    Moved filters to list collection, no more renewing - now collection is reused.

--- ScanApp/Components/Common/Table/Utilities/Filterer.cs
+++ ScanApp/Components/Common/Table/Utilities/Filterer.cs
@@ -21,21 +21,15 @@ namespace ScanApp.Components.Common.Table.Utilities
         /// </returns>
         /// <exception cref="ArgumentNullException"><paramref name="collection"/> is <see langword="null"/>.</exception>
         /// <exception cref="ArgumentNullException">One of <paramref name="filters"/> is <see langword="null"/>.</exception>
-        public static IEnumerable<T> Filter<T>(this IEnumerable<T> collection, params IFilter<T>[] filters)
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> collection, IList<IFilter<T>> filters)
         {
             _ = collection ?? throw new ArgumentNullException(nameof(collection));
             if (filters.IsNullOrEmpty()) return collection;
             if (filters.Any(f => f is null)) throw new ArgumentNullException(nameof(collection), "one of given filters is null.");
 
-            return filters.Length == 1
+            return filters.Count == 1
                 ? filters[0].Run(collection)
-                : collection.AsParallel().Where(item => filters.All(f => f.Check(item)));
-        }
-
-        ///<inheritdoc cref="Filter{T}(System.Collections.Generic.IEnumerable{T},ScanApp.Components.Common.Table.Utilities.IFilter{T}[])"/>
-        public static IEnumerable<T> Filter<T>(this IEnumerable<T> collection, IEnumerable<IFilter<T>> filters)
-        {
-            return Filter(collection, filters?.ToArray());
+                : collection.Where(item => filters.All(f => f.Check(item)));
         }
     }
 }
