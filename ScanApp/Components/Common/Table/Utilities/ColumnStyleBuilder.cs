977217792c1dfa0480a189ad91466097830b5b79
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-17 12:36:45 +0200

    Add docs for Column style builder.
    Also small refactoring - configs are now params of method,
    not constructor.

--- ScanApp/Components/Common/Table/Utilities/ColumnStyleBuilder.cs
+++ ScanApp/Components/Common/Table/Utilities/ColumnStyleBuilder.cs
@@ -6,25 +6,27 @@ using System.Text;
 
 namespace ScanApp.Components.Common.Table.Utilities
 {
+    /// <summary>
+    /// Used to provide custom column styles for <see cref="SCTable{TTableType}"/> in form of <see cref="MarkupString"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of item used in styled <see cref="SCTable{TTableType}"/>.</typeparam>
     public class ColumnStyleBuilder<T>
     {
-        private readonly IEnumerable<ColumnConfig<T>> _configs;
-
-        public ColumnStyleBuilder(IEnumerable<ColumnConfig<T>> configs)
+        /// <summary>
+        /// Creates <see cref="MarkupString"/> containing all provided column styles (if any) to be applied by <see cref="SCTable{TTableType}"/>.
+        /// </summary>
+        /// <param name="configs">Column configuration files used to create custom styles.</param>
+        /// <returns>markup string containing prepared column styles.</returns>
+        public MarkupString BuildUsing(IList<ColumnConfig<T>> configs)
         {
             if (configs?.Any(c => c is null) ?? true)
             {
                 throw new ArgumentNullException(nameof(configs), $"{configs} collections is null or one of configs is null.");
             }
 
-            _configs = configs;
-        }
-
-        public MarkupString Build()
-        {
             var htmlBuilder = new StringBuilder();
 
-            foreach (var columnConfig in _configs)
+            foreach (var columnConfig in configs)
             {
                 if (string.IsNullOrEmpty(columnConfig.ColumnStyle))
                 {
@@ -35,7 +37,7 @@ namespace ScanApp.Components.Common.Table.Utilities
                     htmlBuilder.Append("<col style=\"").Append(columnConfig.ColumnStyle).Append("\" />");
                 }
 
-                if (columnConfig != _configs.Last())
+                if (columnConfig != configs.Last())
                     htmlBuilder.Append('\n');
             }
             return new MarkupString(htmlBuilder.ToString());
