ab5042d9996484f975ffb99e7529a5d28399c1a3
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-18 13:28:42 +0200

    Fixed validation.

--- ScanApp.Common/Validators/ArticleNumberValidator.cs
+++ ScanApp.Common/Validators/ArticleNumberValidator.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using System.Linq;
+using FluentValidation;
+using FluentValidation.Results;
 
 namespace ScanApp.Common.Validators
 {
@@ -12,6 +14,15 @@ namespace ScanApp.Common.Validators
                 .Must(n => !(n[0].Equals(' ') || n[^1].Equals(' ') || n.Contains("  ")))
                 .When(n => string.IsNullOrEmpty(n) is false)
                 .WithMessage("\"{PropertyValue}\" is not a valid article number.");
+        }
+
+        protected override bool PreValidate(ValidationContext<string> context, ValidationResult result)
+        {
+            if (context.InstanceToValidate is not null) return true;
+
+            var name = CreateDescriptor().Rules?.First()?.PropertyName ?? "Field";
+            result.Errors.Add(new ValidationFailure(name, $"{name} cannot be empty."));
+            return false;
         }
     }
 }
