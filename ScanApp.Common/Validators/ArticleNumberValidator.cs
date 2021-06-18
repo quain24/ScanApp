5370647224f510a786c034e88aaed6aeae43469c
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-18 14:10:57 +0200

    Validator update - will use standard messages.

--- ScanApp.Common/Validators/ArticleNumberValidator.cs
+++ ScanApp.Common/Validators/ArticleNumberValidator.cs
@@ -9,12 +9,8 @@ namespace ScanApp.Common.Validators
         public ArticleNumberValidator()
         {
             RuleFor(n => n)
-                .NotEmpty()
-                .WithMessage("Article number cannot be empty")
                 .MinimumLength(5)
-                .WithMessage("Article number is too short. Minimum 5 digits required.")
                 .MaximumLength(20)
-                .WithMessage("Article number is too long. Maximum 20 digits required.")
                 .Must(n => !(n[0].Equals(' ') || n[^1].Equals(' ') || n.Contains("  ")))
                 .When(n => string.IsNullOrEmpty(n) is false)
                 .WithMessage("\"{PropertyValue}\" is not a valid article number.");
