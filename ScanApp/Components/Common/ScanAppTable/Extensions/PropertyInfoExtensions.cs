5f3be38d081efd7be4ef59388c8808a682b368fc
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-06-22 11:20:54 +0200

    Add possibility of choosing how DateTime data is displayed
    Add ItemEdited delegate that is invoked everytime an
    Item is edited successfully.
    Edit ColumnConfig to implement DateTimeFormat.Show

---
+++ ScanApp/Components/Common/ScanAppTable/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using ScanApp.Components.Common.ScanAppTable.Options;
+
+namespace ScanApp.Components.Common.ScanAppTable.Extensions
+{
+    public static class PropertyInfoExtensions
+    {
+        public static object? GetValue(this PropertyInfo propertyInfo, object? obj, DateTimeFormat.Show dateTimeFormat)
+        {
+            if (propertyInfo.PropertyType.IsDateTime())
+            {
+                try
+                {
+                    var date = (DateTime)propertyInfo.GetValue(obj);
+                    return GetDateForm(dateTimeFormat, date);
+                }
+                catch
+                {
+                    return propertyInfo.GetValue(obj);
+                }
+            }
+            return propertyInfo.GetValue(obj);
+        }
+
+        private static object GetDateForm(DateTimeFormat.Show dateShow, DateTime date) =>
+            dateShow switch
+            {
+                DateTimeFormat.Show.DateOnly => date.Day + "/" + date.Month + "/" + date.Year,
+                DateTimeFormat.Show.DateAndTime => date,
+                DateTimeFormat.Show.DayOfWeek => date.DayOfWeek,
+                DateTimeFormat.Show.DayOnly => date.Day,
+                DateTimeFormat.Show.MonthOnly => date.Month,
+                DateTimeFormat.Show.YearOnly => date.Year,
+                DateTimeFormat.Show.TimeOnly => date.Hour + ":" + date.Minute,
+                DateTimeFormat.Show.TimeWithSeconds => date.Hour + ":" + date.Minute + ":" + date.Second,
+                _ => date
+            };
+    }
+}
