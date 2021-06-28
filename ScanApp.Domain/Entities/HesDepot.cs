6bf809676324f9ec6903d08930b2dcdb808afb9b
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-28 13:39:36 +0200

    Added email to HES entity.

--- ScanApp.Domain/Entities/HesDepot.cs
+++ ScanApp.Domain/Entities/HesDepot.cs
@@ -6,6 +6,7 @@ namespace ScanApp.Domain.Entities
     public class HesDepot
     {
         public int Id { get; set; }
+        public string Email { get; private set; }
         public string Name { get; private set; }
         public Address Address { get; private set; }
         public string PhonePrefix { get; private set; }
@@ -18,13 +19,14 @@ namespace ScanApp.Domain.Entities
         {
         }
 
-        public HesDepot(int id, string name, Address address, string phonePrefix, string phoneNumber)
+        public HesDepot(int id, string name, Address address, string phonePrefix, string phoneNumber, string email)
         {
             Id = id;
             ChangeName(name);
             ChangeAddress(address);
             ChangePhonePrefix(phonePrefix);
             ChangePhoneNumber(phoneNumber);
+            ChangeEmail(email);
         }
 
         public void ChangeName(string name)
@@ -48,6 +50,14 @@ namespace ScanApp.Domain.Entities
         {
             if (string.IsNullOrWhiteSpace(number)) throw new ArgumentException("Phone number must be provided", nameof(number));
             PhoneNumber = number;
+        }
+
+        public void ChangeEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email)) throw new ArgumentException("Email address must be provided", nameof(email));
+            if (email.Contains('@') is false && email.Contains('.') is false)
+                throw new ArgumentException($"Email address ({email}) is not a proper email.", nameof(email));
+            Email = email;
         }
     }
 }
