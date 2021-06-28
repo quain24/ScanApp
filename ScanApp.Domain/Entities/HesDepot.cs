952e93dd0c9b0fb17d21f58b104071a75dd49361
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-28 13:36:34 +0200

    New Entity for HES depot.
    In addition - created Address value object.

---
+++ ScanApp.Domain/Entities/HesDepot.cs
@@ -0,0 +1,53 @@
+﻿using ScanApp.Domain.ValueObjects;
+using System;
+
+namespace ScanApp.Domain.Entities
+{
+    public class HesDepot
+    {
+        public int Id { get; set; }
+        public string Name { get; private set; }
+        public Address Address { get; private set; }
+        public string PhonePrefix { get; private set; }
+        public string PhoneNumber { get; private set; }
+
+        /// <summary>
+        /// For compliance with EF Core.
+        /// </summary>
+        private HesDepot()
+        {
+        }
+
+        public HesDepot(int id, string name, Address address, string phonePrefix, string phoneNumber)
+        {
+            Id = id;
+            ChangeName(name);
+            ChangeAddress(address);
+            ChangePhonePrefix(phonePrefix);
+            ChangePhoneNumber(phoneNumber);
+        }
+
+        public void ChangeName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Depot must have name.", nameof(name));
+            Name = name;
+        }
+
+        public void ChangeAddress(Address address)
+        {
+            Address = address ?? throw new ArgumentNullException(nameof(address), $"Provided {nameof(Address)} object was null.");
+        }
+
+        public void ChangePhonePrefix(string prefix)
+        {
+            if (string.IsNullOrWhiteSpace(prefix)) throw new ArgumentException("There must be a phone number prefix given.", nameof(prefix));
+            PhonePrefix = prefix;
+        }
+
+        public void ChangePhoneNumber(string number)
+        {
+            if (string.IsNullOrWhiteSpace(number)) throw new ArgumentException("Phone number must be provided", nameof(number));
+            PhoneNumber = number;
+        }
+    }
+}
---
+++ ScanApp.Domain/ValueObjects/Address.cs
@@ -0,0 +1,62 @@
+﻿using ScanApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace ScanApp.Domain.ValueObjects
+{
+    /// <summary>
+    /// Value object representing basic address (Country, City, Street, Street number, Zip code).
+    /// </summary>
+    public sealed class Address : ValueObject
+    {
+        public string StreetName { get; }
+        public string StreetNumber { get; }
+        public string ZipCode { get; }
+        public string City { get; }
+        public string Country { get; }
+
+        /// <summary>
+        /// Creates new instance of <see cref="Address"/> value object.
+        /// </summary>
+        /// <param name="streetName">Name of the street.</param>
+        /// <param name="streetNumber">Street address number (can be <see langword="null"/> if not present).</param>
+        /// <param name="zipCode">Zip code</param>
+        /// <param name="city">Name of the city.</param>
+        /// <param name="country">Country where given address is located.</param>
+        /// <returns>A new instance of <see cref="Address"/>.</returns>
+        /// <exception cref="ArgumentException">One of parameters is null, empty or whitespace, excluding street number - it can be null, but not empty or whitespace.</exception>
+        public static Address Create(string streetName, string streetNumber, string zipCode, string city, string country)
+        {
+            if (string.IsNullOrWhiteSpace(streetName))
+                throw new ArgumentException("Street name cannot be null or whitespace.", nameof(streetName));
+            if (streetNumber is not null && string.IsNullOrWhiteSpace(streetNumber))
+                throw new ArgumentException("Street number cannot be whitespace - it can either be null or must contain actual data.", nameof(streetNumber));
+            if (string.IsNullOrWhiteSpace(zipCode))
+                throw new ArgumentException("Zip code cannot be null or whitespace.", nameof(zipCode));
+            if (string.IsNullOrWhiteSpace(city))
+                throw new ArgumentException("City name cannot be null or whitespace.", nameof(city));
+            if (string.IsNullOrWhiteSpace(country))
+                throw new ArgumentException("Country name cannot be null or whitespace.", nameof(country));
+
+            return new Address(streetName, streetNumber, zipCode, city, country);
+        }
+
+        private Address(string streetName, string streetNumber, string zipCode, string city, string country)
+        {
+            StreetName = streetName;
+            StreetNumber = streetNumber;
+            ZipCode = zipCode;
+            City = city;
+            Country = country;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return StreetName;
+            yield return StreetNumber;
+            yield return ZipCode;
+            yield return City;
+            yield return Country;
+        }
+    }
+}
