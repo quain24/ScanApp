3f0227973e7d1047b6f420f60e509789fdf4bdb0
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-29 15:33:01 +0200

    New configuration and concurrency check for HES Depots.
    Migration has been applied. To undo (and also delete HES depo table):
    - Update-Database 20210414170239_Spare_Parts_cleanup
    - Remove-Migration (will delete last migration file - Hes-Hub)
    
    To re-apply - if rolled bac migration but not 'remove-migration':
    - Add-Migration -Project "ScanApp.Infrastructure" -StartupProject "ScanApp" -OutputDir Persistence\Migrations "HesHub"
    - Update-Database

---
+++ ScanApp.Infrastructure/Persistence/Migrations/20210629132914_HesHub.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScanApp.Infrastructure.Persistence.Migrations
+{
+    public partial class HesHub : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "hub");
+
+            migrationBuilder.CreateTable(
+                name: "HesDepots",
+                schema: "hub",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    StreetName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    StreetNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    ZipCode = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    PhonePrefix = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Version = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true, comment: "This Row version is converted to 'Version' object in ScanApp")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HesDepots", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HesDepots_Id",
+                schema: "hub",
+                table: "HesDepots",
+                column: "Id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HesDepots",
+                schema: "hub");
+        }
+    }
+}
