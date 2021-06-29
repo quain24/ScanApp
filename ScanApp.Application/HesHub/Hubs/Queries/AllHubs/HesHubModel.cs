b997eaee359b8338d7ee145aeafa8fa6eaa75ceb
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-29 15:36:07 +0200

    Create Mediatr query for all Depots.
    Added dependency on "System.Data.SqlClient" to be able to catch sql exceptions.

---
+++ ScanApp.Application/HesHub/Hubs/Queries/AllHubs/HesHubModel.cs
@@ -0,0 +1,19 @@
+﻿using Version = ScanApp.Domain.ValueObjects.Version;
+
+namespace ScanApp.Application.HesHub.Hubs.Queries.AllHubs
+{
+    public class HesHubModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhonePrefix { get; set; }
+        public string PhoneNumber { get; set; }
+        public Version Version { get; set; }
+    }
+}
