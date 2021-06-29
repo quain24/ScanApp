5e08c652ec319821ebd6fac5bd13e75d815dfa7d
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-29 09:30:41 +0200

    Fix multithreading issues when using sqllite fixture.

--- ScanApp.Tests/UnitTests/SqlLiteInMemoryDbFixture.cs
+++ ScanApp.Tests/UnitTests/SqlLiteInMemoryDbFixture.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using ScanApp.Infrastructure.Persistence;
 
 namespace ScanApp.Tests.UnitTests
@@ -8,23 +9,29 @@ namespace ScanApp.Tests.UnitTests
     public abstract class SqlLiteInMemoryDbFixture : IDisposable
     {
         private const string InMemoryConnectionString = "DataSource=:memory:";
-        private static SqliteConnection _connection;
+        private readonly SqliteConnection _connection;
         private readonly ApplicationDbContext _dbContext;
+        private readonly ServiceProvider _provider;
 
         public ApplicationDbContext NewDbContext => new ApplicationDbContext(Options());
 
         public SqlLiteInMemoryDbFixture()
         {
+            _provider = new ServiceCollection()
+                .AddEntityFrameworkSqlite()
+                .BuildServiceProvider();
+
             _connection = new SqliteConnection(InMemoryConnectionString);
             _connection.Open();
             _dbContext = NewDbContext;
             _dbContext.Database.EnsureCreated();
         }
 
-        private static DbContextOptions<ApplicationDbContext> Options()
+        private DbContextOptions<ApplicationDbContext> Options()
         {
             return new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseSqlite(_connection)
+                .UseInternalServiceProvider(_provider)
                 .Options;
         }
 
