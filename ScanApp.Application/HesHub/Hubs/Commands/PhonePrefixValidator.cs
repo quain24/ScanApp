42918072b5e6e032f24c4f28320ec88de2b3710b
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-30 15:55:04 +0200

    WiP - split validation for re-usability.

---
+++ ScanApp.Application/HesHub/Hubs/Commands/PhonePrefixValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using ScanApp.Common.Validators;
+
+namespace ScanApp.Application.HesHub.Hubs.Commands
+{
+    public class PhonePrefixValidator : AbstractValidator<string>
+    {
+        public PhonePrefixValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .MaximumLength(10)
+                .SetValidator(new PhoneNumberValidator<string, string>());
+        }
+
+        protected override bool PreValidate(ValidationContext<string> context, ValidationResult result)
+        {
+            if (context?.InstanceToValidate is not null)
+                return base.PreValidate(context, result);
+
+            result.Errors.Add(new ValidationFailure(context?.PropertyName, "Phone prefix number cannot be empty."));
+            return false;
+        }
+    }
+}
---
+++ ScanApp.Application/HesHub/Hubs/Commands/StreetNameValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using ScanApp.Common.Validators;
+
+namespace ScanApp.Application.HesHub.Hubs.Commands
+{
+    public class StreetNameValidator : AbstractValidator<string>
+    {
+        public StreetNameValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .MaximumLength(150)
+                .SetValidator(new MustContainOnlyLettersOrAllowedSymbolsValidator<string, string>());
+        }
+
+        protected override bool PreValidate(ValidationContext<string> context, ValidationResult result)
+        {
+            if (context?.InstanceToValidate is not null)
+                return base.PreValidate(context, result);
+
+            result.Errors.Add(new ValidationFailure(context?.PropertyName, "{PropertyName} cannot be empty."));
+            return false;
+        }
+    }
+}
