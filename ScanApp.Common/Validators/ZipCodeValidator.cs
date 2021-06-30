c689df56360c6db6fd9299137603d6ad1c916346
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-30 11:21:55 +0200

    Add basic sanity Zip code validator.

---
+++ ScanApp.Common/Validators/ZipCodeValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System.Text.RegularExpressions;
+
+namespace ScanApp.Common.Validators
+{
+    public class ZipCodeValidator<T, TProperty> : PropertyValidator<T, TProperty>
+    {
+        private readonly Regex _zipCodeBasicRegex = new(@"^[a-z0-9][a-z0-9\- ]{0,10}[a-z0-9]$");
+
+        public override bool IsValid(ValidationContext<T> context, TProperty value)
+        {
+            return value is string s && _zipCodeBasicRegex.Match(s).Success;
+        }
+
+        public override string Name => "ZIP code basic validator";
+    }
+}
