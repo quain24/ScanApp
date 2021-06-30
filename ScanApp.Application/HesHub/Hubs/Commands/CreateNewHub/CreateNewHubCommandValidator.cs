d02170cfa8b5225106c8f91d3cb7700c4c396a39
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-30 15:54:39 +0200

    Add Create hub validator

--- ScanApp.Application/HesHub/Hubs/Commands/CreateNewHub/CreateNewHubCommandValidator.cs
+++ ScanApp.Application/HesHub/Hubs/Commands/CreateNewHub/CreateNewHubCommandValidator.cs
@@ -9,50 +9,7 @@ namespace ScanApp.Application.HesHub.Hubs.Commands.CreateNewHub
         public CreateNewHubCommandValidator()
         {
             RuleFor(x => x.Model)
-                .ChildRules(m =>
-                {
-                    m.RuleFor(x => x.Name)
-                        .NotEmpty()
-                        .MaximumLength(200)
-                        .SetValidator(new MustContainOnlyLettersOrAllowedSymbolsValidator<HesHubModel, string>());
-                    m.RuleFor(x => x.StreetName)
-                        .NotEmpty()
-                        .MaximumLength(150)
-                        .SetValidator(new MustContainOnlyLettersOrAllowedSymbolsValidator<HesHubModel, string>());
-                    m.When(x => x.StreetNumber is not null, () =>
-                    {
-                        m.RuleFor(x => x.StreetNumber)
-                            .NotEmpty()
-                            .MaximumLength(200)
-                            .SetValidator(new EmailValidator<HesHubModel, string>());
-                    });
-                    m.RuleFor(x => x.City)
-                        .NotEmpty()
-                        .MaximumLength(150)
-                        .SetValidator(new MustContainOnlyLettersOrAllowedSymbolsValidator<HesHubModel, string>());
-                    m.RuleFor(x => x.Country)
-                        .NotEmpty()
-                        .MaximumLength(150)
-                        .SetValidator(new MustContainOnlyLettersOrAllowedSymbolsValidator<HesHubModel, string>());
-                    m.RuleFor(x => x.ZipCode)
-                        .NotEmpty()
-                        .MaximumLength(20)
-                        .SetValidator(new ZipCodeValidator<HesHubModel, string>());
-                    m.RuleFor(x => x.Email)
-                        .NotEmpty()
-                        .MaximumLength(200)
-                        .SetValidator(new EmailValidator<HesHubModel, string>());
-                    m.RuleFor(x => x.PhonePrefix)
-                        .NotEmpty()
-                        .MaximumLength(10)
-                        .SetValidator(new PhoneNumberValidator<HesHubModel, string>());
-                    m.RuleFor(x => x.PhoneNumber)
-                        .NotEmpty()
-                        .MaximumLength(25)
-                        .SetValidator(new PhoneNumberValidator<HesHubModel, string>());
-                    m.RuleFor(x => x.Version)
-                        .NotNull();
-                });
+                .SetValidator(new HesHubModelValidator());
         }
 
         protected override bool PreValidate(ValidationContext<CreateNewHubCommand> context, ValidationResult result)
