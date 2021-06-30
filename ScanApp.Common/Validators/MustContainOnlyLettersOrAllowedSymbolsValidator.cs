8d8d853586f362630912d1981c0d4853651d85e4
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-30 09:28:38 +0200

    Updated validator for common address / normal naming.
    Added support for: ,/'":

--- ScanApp.Common/Validators/MustContainOnlyLettersOrAllowedSymbolsValidator.cs
+++ ScanApp.Common/Validators/MustContainOnlyLettersOrAllowedSymbolsValidator.cs
@@ -1,5 +1,5 @@
 ﻿using FluentValidation;
-using FluentValidation.Results;
+using FluentValidation.Validators;
 using System.Text.RegularExpressions;
 
 namespace ScanApp.Common.Validators
@@ -44,24 +44,24 @@ namespace ScanApp.Common.Validators
     /// </summary>
     /// <typeparam name="T">Type of validation context.</typeparam>
     /// <typeparam name="TProperty">Type of property value to validate.</typeparam>
-    public class MustContainOnlyLettersOrAllowedSymbolsValidator : AbstractValidator<string>
+    public class MustContainOnlyLettersOrAllowedSymbolsValidator<T, TProperty> : PropertyValidator<T, TProperty>
     {
         private readonly Regex _allowedCharsRegex = new(@"^[\p{L}0-9\s.\-_:,/'""]+$");
+        public override string Name => "ScanApp allowed chars";
 
-        public MustContainOnlyLettersOrAllowedSymbolsValidator()
+        public override bool IsValid(ValidationContext<T> context, TProperty value)
         {
-            RuleFor(x => x)
-                .NotEmpty()
-                .Matches(_allowedCharsRegex);
+            if (value is string name)
+            {
+                return _allowedCharsRegex.IsMatch(name);
+            }
+
+            return false;
         }
 
-        protected override bool PreValidate(ValidationContext<string> context, ValidationResult result)
+        protected override string GetDefaultMessageTemplate(string errorCode)
         {
-            if (context?.InstanceToValidate is not null)
-                return base.PreValidate(context, result);
-
-            result.Errors.Add(new ValidationFailure(context?.PropertyName, "Value cannot be null."));
-            return false;
+            return "\"{PropertyName}\" field contains illegal symbols.";
         }
     }
 }
