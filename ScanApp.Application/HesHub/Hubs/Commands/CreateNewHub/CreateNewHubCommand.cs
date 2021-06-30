f9aa4ac13507ae46fda49648dc19cf85a3b27778
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-06-30 11:30:34 +0200

    Add create hub command with validator.

---
+++ ScanApp.Application/HesHub/Hubs/Commands/CreateNewHub/CreateNewHubCommand.cs
@@ -0,0 +1,57 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ScanApp.Application.Common.Helpers.Result;
+using ScanApp.Application.Common.Interfaces;
+using ScanApp.Domain.Entities;
+using ScanApp.Domain.ValueObjects;
+using System;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScanApp.Application.HesHub.Hubs.Commands.CreateNewHub
+{
+    public record CreateNewHubCommand(HesHubModel Model) : IRequest<Result>;
+
+    internal class CreateNewHubCommandHandler : IRequestHandler<CreateNewHubCommand, Result>
+    {
+        private readonly IContextFactory _factory;
+
+        public CreateNewHubCommandHandler(IContextFactory factory)
+        {
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+        }
+
+        public async Task<Result> Handle(CreateNewHubCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await using var ctx = _factory.CreateDbContext();
+
+                var model = request.Model;
+                var depot = new HesDepot(model.Id, model.Name,
+                    Address.Create(model.StreetName, model.StreetNumber, model.ZipCode, model.City, model.Country),
+                    model.PhonePrefix, model.PhoneNumber, model.Email);
+
+                await ctx.HesDepots.AddAsync(depot, cancellationToken).ConfigureAwait(false);
+                var saved = await ctx.SaveChangesAsync(cancellationToken);
+
+                return saved == 1 ? new Result() : new Result(ErrorType.Unknown);
+            }
+            catch (OperationCanceledException ex)
+            {
+                return new Result(ErrorType.Cancelled, ex);
+            }
+            catch (DbUpdateException ex)
+            {
+                return ex is DbUpdateConcurrencyException
+                    ? new Result(ErrorType.ConcurrencyFailure, ex.InnerException?.Message ?? ex.Message, ex)
+                    : new Result(ErrorType.DatabaseError, ex.InnerException?.Message ?? ex.Message, ex);
+            }
+            catch (SqlException ex)
+            {
+                return new Result(ErrorType.DatabaseError, ex.InnerException?.Message ?? ex.Message, ex);
+            }
+        }
+    }
+}
---
+++ ScanApp.Application/HesHub/Hubs/Commands/CreateNewHub/CreateNewHubCommandValidator.cs
@@ -0,0 +1,67 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using ScanApp.Common.Validators;
+
+namespace ScanApp.Application.HesHub.Hubs.Commands.CreateNewHub
+{
+    public class CreateNewHubCommandValidator : AbstractValidator<CreateNewHubCommand>
+    {
+        public CreateNewHubCommandValidator()
+        {
+            RuleFor(x => x.Model)
+                .ChildRules(m =>
+                {
+                    m.RuleFor(x => x.Name)
+                        .NotEmpty()
+                        .MaximumLength(200)
+                        .SetValidator(new MustContainOnlyLettersOrAllowedSymbolsValidator<HesHubModel, string>());
+                    m.RuleFor(x => x.StreetName)
+                        .NotEmpty()
+                        .MaximumLength(150)
+                        .SetValidator(new MustContainOnlyLettersOrAllowedSymbolsValidator<HesHubModel, string>());
+                    m.When(x => x.StreetNumber is not null, () =>
+                    {
+                        m.RuleFor(x => x.StreetNumber)
+                            .NotEmpty()
+                            .MaximumLength(200)
+                            .SetValidator(new EmailValidator<HesHubModel, string>());
+                    });
+                    m.RuleFor(x => x.City)
+                        .NotEmpty()
+                        .MaximumLength(150)
+                        .SetValidator(new MustContainOnlyLettersOrAllowedSymbolsValidator<HesHubModel, string>());
+                    m.RuleFor(x => x.Country)
+                        .NotEmpty()
+                        .MaximumLength(150)
+                        .SetValidator(new MustContainOnlyLettersOrAllowedSymbolsValidator<HesHubModel, string>());
+                    m.RuleFor(x => x.ZipCode)
+                        .NotEmpty()
+                        .MaximumLength(20)
+                        .SetValidator(new ZipCodeValidator<HesHubModel, string>());
+                    m.RuleFor(x => x.Email)
+                        .NotEmpty()
+                        .MaximumLength(200)
+                        .SetValidator(new EmailValidator<HesHubModel, string>());
+                    m.RuleFor(x => x.PhonePrefix)
+                        .NotEmpty()
+                        .MaximumLength(10)
+                        .SetValidator(new PhoneNumberValidator<HesHubModel, string>());
+                    m.RuleFor(x => x.PhoneNumber)
+                        .NotEmpty()
+                        .MaximumLength(25)
+                        .SetValidator(new PhoneNumberValidator<HesHubModel, string>());
+                    m.RuleFor(x => x.Version)
+                        .NotNull();
+                });
+        }
+
+        protected override bool PreValidate(ValidationContext<CreateNewHubCommand> context, ValidationResult result)
+        {
+            if (context?.InstanceToValidate is not null)
+                return base.PreValidate(context, result);
+
+            result.Errors.Add(new ValidationFailure(context?.PropertyName, $"Given {nameof(CreateNewHubCommand)} was null."));
+            return false;
+        }
+    }
+}
