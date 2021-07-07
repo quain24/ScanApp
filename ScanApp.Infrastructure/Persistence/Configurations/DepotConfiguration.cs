229a26e5566e5b581ddd7febfa24eaf724c9d1a4
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-07 15:58:38 +0200

    Add entity configurations and required migration.

--- ScanApp.Infrastructure/Persistence/Configurations/DepotConfiguration.cs
+++ ScanApp.Infrastructure/Persistence/Configurations/DepotConfiguration.cs
@@ -12,7 +12,7 @@ namespace ScanApp.Infrastructure.Persistence.Configurations
         public void Configure(EntityTypeBuilder<Depot> builder)
         {
             builder.ToTable("Depots", "hub");
-            builder.HasIndex(e => e.Id).IsUnique();
+            builder.HasKey(e => e.Id);
             builder.Property(e => e.Id).ValueGeneratedNever().IsRequired();
             builder.Property(e => e.Name).HasMaxLength(200).IsRequired();
             builder.Property(e => e.Email).HasMaxLength(200).IsRequired();
@@ -24,6 +24,14 @@ namespace ScanApp.Infrastructure.Persistence.Configurations
                 add.Property(a => a.Country).HasColumnName("Country").HasMaxLength(150).IsRequired();
                 add.Property(a => a.ZipCode).HasColumnName("ZipCode").HasMaxLength(20).IsRequired();
             }).Navigation(e => e.Address).IsRequired();
+
+            builder.HasOne(e => e.DefaultTrailer)
+                .WithMany()
+                .IsRequired(false);
+
+            builder.HasOne(e => e.DefaultGate)
+                .WithMany()
+                .IsRequired(false);
 
             builder.Property(e => e.Version)
                 .HasComment("This Row version is converted to 'Version' object in ScanApp")
---
+++ ScanApp.Infrastructure/Persistence/Configurations/TrailerConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using ScanApp.Domain.Entities;
+using SharedExtensions;
+using System;
+using Version = ScanApp.Domain.ValueObjects.Version;
+
+namespace ScanApp.Infrastructure.Persistence.Configurations
+{
+    public class TrailerConfiguration : IEntityTypeConfiguration<Trailer>
+    {
+        public void Configure(EntityTypeBuilder<Trailer> builder)
+        {
+            builder.ToTable("Trailers", "hub");
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(e => e.LoadingTime)
+                .HasConversion(new TimeSpanToStringConverter());
+            builder.Property(e => e.UnloadingTime)
+                .HasConversion(new TimeSpanToStringConverter());
+
+            builder.Property(e => e.Version)
+                .HasComment("This Row version is converted to 'Version' object in ScanApp")
+                .IsRowVersion()
+                .HasConversion(c => c.IsEmpty ? null : Convert.FromBase64String(c.Value),
+                    x => x.IsNullOrEmpty() ? Version.Empty() : Version.Create(Convert.ToBase64String(x)));
+        }
+    }
+}
