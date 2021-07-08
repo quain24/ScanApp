5938ca28da7e55c97e4f7fd43daa001a56a32b6b
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-08 15:25:21 +0200

    Fix - line number reader did not provide correct number.
    This was due to execution of reader method by property.
    Instead, method must be directly executed.
    
    Added simple Line number reader test.

---
+++ ScanApp.Tests/UnitTests/Common/Helpers/LineNumberTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using ScanApp.Common.Helpers;
+using Xunit;
+
+namespace ScanApp.Tests.UnitTests.Common.Helpers
+{
+    public class LineNumberTests
+    {
+        [Fact]
+        public void Gets_file_lineNumber_on_which_it_was_called()
+        {
+            var result = LineNumber.Get();
+            result.Should().Be(12);
+        }
+    }
+}
