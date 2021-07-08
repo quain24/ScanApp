4cfad12c432e20199f0cf9f895d6118ab22e8bb7
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-08 15:06:53 +0200

    Fix - zip code now accepts capital letters.

--- ScanApp.Common/Validators/ZipCodeValidator.cs
+++ ScanApp.Common/Validators/ZipCodeValidator.cs
@@ -1,17 +1,29 @@
 ﻿using FluentValidation;
+using FluentValidation.Results;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace ScanApp.Common.Validators
 {
     public class ZipCodeValidator : AbstractValidator<string>
     {
-        private readonly Regex _zipCodeBasicRegex = new(@"^[a-z0-9][a-z0-9\- ]{0,10}[a-z0-9]$");
+        private readonly Regex _zipCodeBasicRegex = new(@"^[A-Za-z0-9][a-z0-9\- ]{0,10}[A-Za-z0-9]$");
 
         public ZipCodeValidator()
         {
             RuleFor(x => x)
                 .NotEmpty()
+                .Must(x => x.Contains("  ") is false)
                 .Matches(_zipCodeBasicRegex);
+        }
+
+        protected override bool PreValidate(ValidationContext<string> context, ValidationResult result)
+        {
+            if (context.InstanceToValidate is not null) return true;
+
+            var name = CreateDescriptor().Rules?.First()?.PropertyName ?? "Field";
+            result.Errors.Add(new ValidationFailure(name, $"{name} cannot be empty."));
+            return false;
         }
     }
 }
