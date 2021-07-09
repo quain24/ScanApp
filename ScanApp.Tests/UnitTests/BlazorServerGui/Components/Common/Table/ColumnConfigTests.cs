51bac6f377fb70f170b19d453afccb243f1085b7
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-09 12:43:23 +0200

    Quick fix after rebase.

--- ScanApp.Tests/UnitTests/BlazorServerGui/Components/Common/Table/ColumnConfigTests.cs
+++ ScanApp.Tests/UnitTests/BlazorServerGui/Components/Common/Table/ColumnConfigTests.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using ScanApp.Common;
 using ScanApp.Components.Common.Table;
 using Xunit;
 using Xunit.Abstractions;
