2147bde27880f06330a28d15488e037e09845b6e
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-19 17:19:43 +0200

    Optimized 'last admin' check when executing remove from role command.
    Initial unit test fixup.

--- ScanApp.Application/Admin/Commands/RemoveUserFromRole/RemoveUserFromRoleCommand.cs
+++ ScanApp.Application/Admin/Commands/RemoveUserFromRole/RemoveUserFromRoleCommand.cs
@@ -1,9 +1,12 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using ScanApp.Application.Common.Helpers.Result;
 using ScanApp.Application.Common.Interfaces;
 using System;
+using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
 using Version = ScanApp.Domain.ValueObjects.Version;
 
 namespace ScanApp.Application.Admin.Commands.RemoveUserFromRole
@@ -21,27 +24,58 @@ namespace ScanApp.Application.Admin.Commands.RemoveUserFromRole
     {
         private readonly IUserManager _userManager;
         private readonly IRoleManager _roleManager;
+        private readonly IContextFactory _factory;
 
-        public RemoveUserFromRoleCommandHandler(IUserManager userManager, IRoleManager roleManager)
+        public RemoveUserFromRoleCommandHandler(IUserManager userManager, IRoleManager roleManager, IContextFactory factory)
         {
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
             _roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
         }
 
         public async Task<Result<Version>> Handle(RemoveUserFromRoleCommand request, CancellationToken cancellationToken)
         {
-            if (request.RoleName.Equals(Globals.RoleNames.Administrator))
+            try
             {
-                var adminUsers = await _roleManager.UsersInRole(Globals.RoleNames.Administrator, cancellationToken).ConfigureAwait(false);
-                if (adminUsers.Count == 1 && adminUsers[0].Equals(request.UserName, StringComparison.OrdinalIgnoreCase))
-                {
-                    return new Result<Version>(ErrorType.IllegalAccountOperation,
-                        $"Cannot remove user from {Globals.RoleNames.Administrator} role - no more users with " +
-                        $"{Globals.RoleNames.Administrator} role would be left.");
-                }
-            }
+                await using var context = _factory.CreateDbContext();
+                var strategy = context.Database.CreateExecutionStrategy();
 
-            return await _userManager.RemoveUserFromRole(request.UserName, request.Version, request.RoleName);
+                return await strategy.ExecuteAsync(async token =>
+                {
+                    using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+                    await using var ctx = _factory.CreateDbContext();
+                    token.ThrowIfCancellationRequested();
+                    var result = await _userManager.RemoveUserFromRole(request.UserName, request.Version, request.RoleName).ConfigureAwait(false);
+
+                    if (request.RoleName.Equals(Globals.RoleNames.Administrator))
+                    {
+                        var adminUsers = await _roleManager.UsersInRole(Globals.RoleNames.Administrator, cancellationToken).ConfigureAwait(false);
+                        if (adminUsers.Count == 0)
+                        {
+                            return new Result<Version>(ErrorType.IllegalAccountOperation,
+                                $"Cannot remove user from {Globals.RoleNames.Administrator} role - no more users with " +
+                                $"{Globals.RoleNames.Administrator} role would be left.").SetOutput(result.Output);
+                        }
+                    }
+
+                    scope.Complete();
+                    return result;
+                }, cancellationToken);
+            }
+            catch (OperationCanceledException ex)
+            {
+                return new Result<Version>(ErrorType.Cancelled, ex).SetOutput(request.Version);
+            }
+            catch (DbUpdateException ex)
+            {
+                return ex is DbUpdateConcurrencyException
+                    ? new Result<Version>(ErrorType.ConcurrencyFailure, ex.InnerException?.Message ?? ex.Message, ex).SetOutput(request.Version)
+                    : new Result<Version>(ErrorType.DatabaseError, ex.InnerException?.Message ?? ex.Message, ex).SetOutput(request.Version);
+            }
+            catch (SqlException ex)
+            {
+                return new Result<Version>(ErrorType.DatabaseError, ex.InnerException?.Message ?? ex.Message, ex).SetOutput(request.Version);
+            }
         }
     }
 }
