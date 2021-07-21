68135785e3381e68057de8b8e500dd284b53c16e
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-07-21 11:23:06 +0200

    Add test Radzen calendar component

---
+++ ScanApp.Common/Validators/AppointmentTitleValidator.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ScanApp.Common.Validators
+{
+    public class AppointmentTitleValidator : AbstractValidator<string>
+    {
+        public AppointmentTitleValidator()
+        {
+            RuleFor(x => x)
+                .MinimumLength(3)
+                .WithMessage("Title must contain at least 3 characters.")
+                .MaximumLength(100)
+                .WithMessage("The title cannot be longer than 100 characters.");
+        }
+
+        protected override bool PreValidate(ValidationContext<string> context, ValidationResult result)
+        {
+            if (context.InstanceToValidate is not null) return true;
+
+            var name = CreateDescriptor().Rules?.First()?.PropertyName ?? "Field";
+            result.Errors.Add(new ValidationFailure(name, $"{name} cannot be empty."));
+            return false;
+        }
+    }
+}
---
+++ ScanApp/Models/Calendar/Appointment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScanApp.Models.Calendar
+{
+    public class Appointment
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Text { get; set; }
+        public bool Important { get; set; }
+    }
+}
