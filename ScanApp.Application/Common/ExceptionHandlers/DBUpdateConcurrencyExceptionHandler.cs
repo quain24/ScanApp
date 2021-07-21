9f15113cea40392bb1cd8f6f15ed6d3d4c9c833c
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-21 16:07:50 +0200

    Refactored db update concurrency exception handler.
    It now, as rest of handlers dealing with children of db update exception,
    extends db update exception handler.
    Also, changed message format.

--- ScanApp.Application/Common/ExceptionHandlers/DBUpdateConcurrencyExceptionHandler.cs
+++ ScanApp.Application/Common/ExceptionHandlers/DBUpdateConcurrencyExceptionHandler.cs
@@ -1,23 +1,31 @@
 ﻿using MediatR;
 using MediatR.Pipeline;
 using Microsoft.EntityFrameworkCore;
+using ScanApp.Application.Common.ExceptionHandlers.GeneralHandlers;
 using ScanApp.Application.Common.Helpers.Result;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ScanApp.Application.Common.ExceptionHandlers
 {
-    public class DBUpdateConcurrencyExceptionHandler<TRequest, TResponse, TException> : IRequestExceptionHandler<TRequest, TResponse, TException>
+    public class DBUpdateConcurrencyExceptionHandler<TRequest, TResponse, TException> : DbUpdateExceptionHandler<TRequest, TResponse, TException>
         where TRequest : IRequest<TResponse>
         where TResponse : Result, new()
         where TException : DbUpdateConcurrencyException
     {
-        public Task Handle(TRequest request, TException exception, RequestExceptionHandlerState<TResponse> state,
+        public override Task Handle(TRequest request, TException exception, RequestExceptionHandlerState<TResponse> state,
             CancellationToken cancellationToken)
         {
             var response = new TResponse();
-            var name = request.GetType().Name;
-            response.Set(ErrorType.ConcurrencyFailure, $"{name} - {exception.InnerException?.Message ?? exception.Message}.", exception);
+            var requestName = request.GetType().Name;
+            var entityNames = string.Join(", ", exception.Entries.Select(x => x.Metadata.Name));
+            var message = new StringBuilder(requestName).Append(" - ")
+                .Append(exception.Message).Append(", ")
+                .Append(entityNames).Append(": ")
+                .Append(exception.InnerException?.Message);
+            response.Set(ErrorType.ConcurrencyFailure, message.ToString(), exception);
             state.SetHandled(response);
             return Task.CompletedTask;
         }
--- ScanApp.Application/Common/ExceptionHandlers/GeneralHandlers/SqlExceptionHandler.cs
+++ ScanApp.Application/Common/ExceptionHandlers/GeneralHandlers/SqlExceptionHandler.cs
@@ -3,10 +3,11 @@ using MediatR.Pipeline;
 using ScanApp.Application.Common.Helpers.Result;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace ScanApp.Application.Common.ExceptionHandlers
+namespace ScanApp.Application.Common.ExceptionHandlers.GeneralHandlers
 {
     public class SqlExceptionHandler<TRequest, TResponse, TException> : IRequestExceptionHandler<TRequest, TResponse, TException>
         where TRequest : IRequest<TResponse>
@@ -21,7 +22,9 @@ namespace ScanApp.Application.Common.ExceptionHandlers
 
             var response = new TResponse();
             var name = request.GetType().Name;
-            response.Set(ErrorType.DatabaseError, $"{name} - {exc.Number} - {exc.Message}.", exc);
+            var errors = string.Join("\r\n", exc.Errors.Cast<SqlError>().Select(x => x.Message));
+
+            response.Set(ErrorType.DatabaseError, $"{name} - {exc.Number} - {exc.Message}\n\r{errors}", exc);
             state.SetHandled(response);
             return Task.CompletedTask;
         }
