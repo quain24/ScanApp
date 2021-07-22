64625e2910695ffaac62c3fe4ab6ff1971d4aee9
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-22 14:43:33 +0200

    Complete rework of sql lite fixture.
    It is now extendale and contains service provider with preconfigured db context factories.
    
    Added also DbFixture with logging and mediator framework to test exception handlers.

---
+++ ScanApp.Tests/IntegrationTests/Application/DbFixtureWithExtensionHandlers.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using MediatR.Pipeline;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using ScanApp.Application.Common.Behaviors;
+using ScanApp.Application.Common.Installers;
+using Serilog;
+using Serilog.Events;
+using Xunit.Abstractions;
+
+namespace ScanApp.Tests.IntegrationTests.Application
+{
+    public abstract class DbFixtureWithExtensionHandlers : SqlLiteInMemoryDbFixture
+    {
+        public ITestOutputHelper Output { get; set; }
+
+        protected override void ConfigureServices(ServiceCollection services)
+        {
+            base.ConfigureServices(services);
+            services.AddMediatR(typeof(MediatRInstaller));
+            services.AddSingleton<IHttpContextAccessor>(Mock.Of<IHttpContextAccessor>());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestExceptionProcessorBehavior<,>));
+            services.AddLogging(c => c.AddSerilog(new LoggerConfiguration()
+                .WriteTo.TestOutput(Output)
+                .Enrich.FromLogContext()
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Error)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
+                .CreateLogger()));
+        }
+    }
+}
---
+++ ScanApp.Tests/SqlLiteInMemoryDbFixture.cs
@@ -0,0 +1,105 @@
+﻿using EntityFramework.Exceptions.Sqlite;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ScanApp.Application.Common.Interfaces;
+using ScanApp.Infrastructure.Persistence;
+using ScanApp.Infrastructure.Services;
+using System;
+
+namespace ScanApp.Tests
+{
+    public abstract class SqlLiteInMemoryDbFixture : IDisposable
+    {
+        private ServiceCollection _serviceCollection;
+
+        /// <summary>
+        /// Service collection of this instance. It can be modified until first actual call for <see cref="Provider"/> or <see cref="NewDbContext"/> was made.
+        /// </summary>
+        public ServiceCollection ServiceCollection
+        {
+            get
+            {
+                if (_serviceCollection is null)
+                {
+                    _serviceCollection = new ServiceCollection();
+                    ConfigureServices(_serviceCollection);
+                }
+                return _serviceCollection;
+            }
+        }
+
+        private ServiceProvider _provider;
+
+        /// <summary>
+        /// Gets the configured service provider.<para/>
+        /// You <b>cannot</b> reconfigure services by using <see cref="ServiceCollection"/> after call for provider has been made.
+        /// </summary>
+        public ServiceProvider Provider
+        {
+            get
+            {
+                if (_provider is null)
+                    InitializeDatabase();
+                return _provider;
+            }
+            private set => _provider = value;
+        }
+
+        /// <summary>
+        /// Gets new instance of configured DbContext.
+        /// You <b>cannot</b> reconfigure services by using <see cref="ServiceCollection"/> after call for NewDbContext has been made.
+        /// </summary>
+        public ApplicationDbContext NewDbContext
+        {
+            get
+            {
+                if (Provider is null)
+                    InitializeDatabase();
+                return Provider.GetService<ApplicationDbContext>();
+            }
+        }
+
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private SqliteConnection _connection;
+        private ApplicationDbContext _dbContext;
+
+        private void InitializeDatabase()
+        {
+            Provider = ServiceCollection.BuildServiceProvider();
+            _connection = new SqliteConnection(InMemoryConnectionString);
+            _connection.Open();
+            _dbContext = NewDbContext;
+            _dbContext.Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Override when additional service registration is needed.<br/>
+        /// If you want to keep pre-configured SqlLite database, call this base method in your override.<para/>
+        /// Mind that <see cref="Provider"/> object is not available for use in this method - it is instantiated after this method has ran to completion.
+        /// </summary>
+        /// <param name="services">Internally provided instance of service collection for use throughout this <see cref="SqlLiteInMemoryDbFixture"/> instance life.</param>
+        protected virtual void ConfigureServices(ServiceCollection services)
+        {
+            // UseExceptionProcessor() - replace standard EF Core exception with more detailed ones. (EntityFramework.Exceptions package for SqlLite)
+            services
+                .AddDbContext<ApplicationDbContext>(o =>
+                        o.UseSqlite(_connection).UseExceptionProcessor(),
+                    contextLifetime: ServiceLifetime.Transient,
+                    optionsLifetime: ServiceLifetime.Singleton);
+
+            services.AddDbContextFactory<ApplicationDbContext>(o =>
+                    o.UseSqlite(_connection).UseExceptionProcessor(),
+                ServiceLifetime.Transient);
+
+            services.AddSingleton<IContextFactory, AppDbContextFactory>(srv =>
+                new AppDbContextFactory(srv.GetRequiredService<IDbContextFactory<ApplicationDbContext>>()));
+        }
+
+        public void Dispose()
+        {
+            _dbContext?.Database.EnsureDeleted();
+            _connection?.Close();
+        }
+    }
+}
