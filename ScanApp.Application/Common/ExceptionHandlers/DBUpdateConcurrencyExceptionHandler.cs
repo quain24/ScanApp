61557986defb7a7c5008a82f3a9035a08c61cc8e
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-23 12:04:55 +0200

    Fix message schema for DbUpdate exception handler

--- ScanApp.Application/Common/ExceptionHandlers/DBUpdateConcurrencyExceptionHandler.cs
+++ ScanApp.Application/Common/ExceptionHandlers/DBUpdateConcurrencyExceptionHandler.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace ScanApp.Application.Common.ExceptionHandlers
 {
-    public class DBUpdateConcurrencyExceptionHandler<TRequest, TResponse, TException> : DbUpdateExceptionHandler<TRequest, TResponse, TException>
+    public class DbUpdateConcurrencyExceptionHandler<TRequest, TResponse, TException> : DbUpdateExceptionHandler<TRequest, TResponse, TException>
         where TRequest : IRequest<TResponse>
         where TResponse : Result, new()
         where TException : DbUpdateConcurrencyException
@@ -22,9 +22,12 @@ namespace ScanApp.Application.Common.ExceptionHandlers
             var requestName = request.GetType().Name;
             var entityNames = string.Join(", ", exception.Entries.Select(x => x.Metadata.Name));
             var message = new StringBuilder(requestName).Append(" - ")
-                .Append(exception.Message).Append(", ")
-                .Append(entityNames).Append(": ")
-                .Append(exception.InnerException?.Message);
+                .Append(entityNames).Append(" - ")
+                .Append(exception.Message);
+            if (exception.InnerException is not null)
+            {
+                message.Append(",\r\n").Append(exception.InnerException.Message);
+            }
             response.Set(ErrorType.ConcurrencyFailure, message.ToString(), exception);
             state.SetHandled(response);
             return Task.CompletedTask;
