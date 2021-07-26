cb0db2a8c407c21822ff7ce780fa19624a64a12a
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-07-26 11:57:55 +0200

    Add editing feature

---
+++ ScanApp/Components/Scheduler/TimeSlot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ScanApp.Components.Scheduler
+{
+    public class TimeSlot
+    {
+        public int StartHour { get; private set; }
+        public int EndHour { get; private set; }
+
+        public override string ToString()
+        {
+            return StartHour.ToString() + ":00 - " + EndHour.ToString() +
+                   ":00";
+        }
+
+        public TimeSlot(int startHour, int endHour)
+        {
+            if (endHour - startHour != 1)
+            {
+                throw new ArgumentException("Difference between start and end hour must be 1.");
+            }
+
+            StartHour = startHour;
+            EndHour = endHour;
+        }
+    }
+}
--- ScanApp/Models/Scheduler/HesAppointmentModel.cs
+++ ScanApp/Models/Scheduler/HesAppointmentModel.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ScanApp.Components.Scheduler;
+using System;
 
 namespace ScanApp.Models.Scheduler
 {
@@ -12,10 +10,21 @@ namespace ScanApp.Models.Scheduler
         public string Company { get; set; }
         public string IdentificationNumber { get; set; }
         public string Note { get; set; }
-        public DateTime FromDate { get; set; }
-        public DateTime ToDate { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSlot TimeSlot { get; set; }
         public bool Loading { get; set; }
         public bool Unloading { get; set; }
         public int GridLocation { get; set; }
+
+        public HesAppointmentModel(DateTime date, int hourFrom, int hourTo)
+        {
+            if (date == new DateTime())
+                throw new ArgumentException("Value must be specified", nameof(Date));
+
+            Date = date;
+
+            TimeSlot = new TimeSlot(hourFrom, hourTo);
+        }
+
     }
 }
