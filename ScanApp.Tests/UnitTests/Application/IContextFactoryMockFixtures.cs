ad604bf58dede89e6dfe35ea9c34cda13782cba6
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-26 10:09:39 +0200

    Context factory mock now auto-creates default values for dbsets when  needed.

--- ScanApp.Tests/UnitTests/Application/IContextFactoryMockFixtures.cs
+++ ScanApp.Tests/UnitTests/Application/IContextFactoryMockFixtures.cs
@@ -1,5 +1,11 @@
-﻿using Moq;
+﻿using Microsoft.EntityFrameworkCore;
+using MockQueryable.Moq;
+using Moq;
 using ScanApp.Application.Common.Interfaces;
+using ScanApp.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ScanApp.Tests.UnitTests.Application
 {
@@ -12,8 +18,25 @@ namespace ScanApp.Tests.UnitTests.Application
         {
             ContextFactoryMock = new Mock<IContextFactory>();
             ContextMock = premadeContextMock ?? new Mock<IApplicationDbContext>();
+            ContextMock.DefaultValueProvider = new DefaultDbContextValueProvider();
 
             ContextFactoryMock.Setup(m => m.CreateDbContext()).Returns(ContextMock.Object);
+        }
+    }
+
+    public class DefaultDbContextValueProvider : DefaultValueProvider
+    {
+        protected override object GetDefaultValue(Type type, Mock mock)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(DbSet<>))
+            {
+                var t = type.GetGenericArguments().First();
+                dynamic dbSet = Activator.CreateInstance(typeof(List<>).MakeGenericType(t));
+                var queryable = Queryable.AsQueryable(dbSet);
+                return MoqExtensions.BuildMockDbSet(queryable).Object;
+            }
+
+            return type.GetDefaultValue();
         }
     }
 }
