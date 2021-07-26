7ac57e86d94bf353e2c29a7c2f1476e055422d4f
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-07-26 15:32:36 +0200

    Simplify HesAppointmentModel class

--- ScanApp/Models/Scheduler/HesAppointmentModel.cs
+++ ScanApp/Models/Scheduler/HesAppointmentModel.cs
@@ -9,18 +9,16 @@ namespace ScanApp.Models.Scheduler
         public string Spedition { get; set; }
         public string Company { get; set; }
         public string IdentificationNumber { get; set; }
-        public string Note { get; set; }
         public DateTime? Date { get; set; }
         public TimeSlot TimeSlot { get; set; }
-        public bool Loading { get; set; }
-        public bool Unloading { get; set; }
-        public int GridLocation { get; set; }
 
-        public HesAppointmentModel(DateTime date, int hourFrom, int hourTo)
+        public HesAppointmentModel(DateTime date, int hourFrom, int hourTo, int depotID)
         {
             if (date == new DateTime())
                 throw new ArgumentException("Value must be specified", nameof(Date));
-
+            if (depotID <= 0)
+                throw new ArgumentException("DepotID cannot be equal or smaller than 0.", nameof(depotID));
+            DepotID = depotID;
             Date = date;
 
             TimeSlot = new TimeSlot(hourFrom, hourTo);
