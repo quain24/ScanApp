4cd441b9f33a6a38fab1f6a9c085d8d4d7ca4327
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-27 12:52:04 +0200

    Restore test menu and services.

--- ScanApp/Startup.cs
+++ ScanApp/Startup.cs
@@ -9,6 +9,7 @@ using ScanApp.Application.Common.Entities;
 using ScanApp.Application.Common.Installers;
 using ScanApp.Common.Installers;
 using ScanApp.Common.Services;
+using ScanApp.Data;
 using ScanApp.Infrastructure.Common.Installers;
 using Serilog;
 
@@ -46,6 +47,7 @@ namespace ScanApp
             services.AddHttpContextAccessor();
             services.AddValidatorsFromAssemblies(new[] { typeof(ApplicationUser).Assembly, typeof(DateTimeService).Assembly });
             services.AddCommonFluentValidationPropertyValidators();
+            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
