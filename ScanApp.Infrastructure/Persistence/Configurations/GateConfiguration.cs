07c71511df063b84fc318d5dd5ae095840ca0ad0
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-27 15:54:55 +0200

    Extract Version converter - avoid duplication when creating version-able entities.

--- ScanApp.Infrastructure/Persistence/Configurations/GateConfiguration.cs
+++ ScanApp.Infrastructure/Persistence/Configurations/GateConfiguration.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using ScanApp.Domain.Entities;
-using SharedExtensions;
-using System;
-using Version = ScanApp.Domain.ValueObjects.Version;
 
 namespace ScanApp.Infrastructure.Persistence.Configurations
 {
@@ -18,8 +15,7 @@ namespace ScanApp.Infrastructure.Persistence.Configurations
             builder.Property(e => e.Version)
                 .HasComment("This Row version is converted to 'Version' object in ScanApp")
                 .IsRowVersion()
-                .HasConversion(c => c.IsEmpty ? null : Convert.FromBase64String(c.Value),
-                    x => x.IsNullOrEmpty() ? Version.Empty : Version.Create(Convert.ToBase64String(x)));
+                .HasConversion(new VersionConverter());
         }
     }
 }
