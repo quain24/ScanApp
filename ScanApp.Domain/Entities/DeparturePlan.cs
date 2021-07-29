a40a066efd74ea956eaf2d75ba272a3b5ac5d71f
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-29 21:30:02 +0200

    Additional notes - departure plan.

--- ScanApp.Domain/Entities/DeparturePlan.cs
+++ ScanApp.Domain/Entities/DeparturePlan.cs
@@ -1,5 +1,6 @@
 ﻿using ScanApp.Domain.ValueObjects;
 using System;
+using System.Collections.Generic;
 
 namespace ScanApp.Domain.Entities
 {
@@ -7,7 +8,9 @@ namespace ScanApp.Domain.Entities
     {
         public int Id { get; set; }
 
-        public Depot Depot { get; set; }
+        public Depot Depots { get; set; }
+
+        public List<string> Modes { get; private set; } = new();
 
         public TrailerType TrailerType { get; set; }
 
@@ -19,18 +22,19 @@ namespace ScanApp.Domain.Entities
         public DayAndTime ArrivalTimeAtDepot { get; set; }
 
         //todo Temporary - placeholder for mode entity
-        public string Mode { get; set; }
     }
 }
 
 /* Departure plan have one or many Modes (Default, Christmas, etc) - Same plan for multiple Modes / one mode can have multiple plans = many to many
  * Departure plan must have at least one mode - make "default" non-deletable in modes table and force plan to have one.
- * Departure plan have one or many Depot assigned. Plan to depot = many to many: one depot can have many plans, one plan can have many depots
- * Combination of Mode and Depot for each departure plan must be unique?
+ * Departure plan have one Depot assigned. Plan to depot = many to one: one depot can have many plans, one plan can have only one depot.
+ * Combination of Mode and Depot for each departure plan must be unique.
  * -   Plan A for Default mode for Depot 56   - OK
- * -   Plan B for Default mode for Depot 56   - ERROR (?) - duplication of mode + depot, even if gate / trailer / other settings are different.
- * -   Plan A for Default mode for Depot 100  - OK (?) - plan settings are the same, just target depot is different.
+ * -   Plan B for Default mode for Depot 56   - OK    - Mode (like Christmas) can have multiple departures to single depot.
+ * -   Plan A for Default mode for Depot 100  - ERROR - Single plan is only for single depot - otherwise 2 depots would try to load at the same time on the same gate 
  * -   Plan B for Christmas mode for Depot 56 - OK
+ *
+ *  >>>--- Detect collision Gate / time For plans in one mode ---<<<
  *
  * One departure plan -> many Depots
  * One departure plan -> many Modes
