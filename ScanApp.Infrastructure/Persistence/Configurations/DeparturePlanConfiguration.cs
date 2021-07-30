9e9550eae52446f4538a1242c5f5009fe9aabce2
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-07-30 16:11:50 +0200

    Primary logic in Departure plan configuration.

--- ScanApp.Infrastructure/Persistence/Configurations/DeparturePlanConfiguration.cs
+++ ScanApp.Infrastructure/Persistence/Configurations/DeparturePlanConfiguration.cs
@@ -9,6 +9,30 @@ namespace ScanApp.Infrastructure.Persistence.Configurations
     {
         public override void Configure(EntityTypeBuilder<DeparturePlan> builder)
         {
+            builder.ToTable("DeparturePlans", "hub");
+
+            builder.HasOne(x => x.Depot)
+                .WithMany()
+                .IsRequired()
+                .HasForeignKey("DepotId")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasIndex("DepotId", nameof(DeparturePlan.Id))
+                .IsUnique();
+
+            builder.HasMany(x => x.Seasons)
+                .WithMany("DeparturePlans");
+
+            builder.HasOne(x => x.TrailerType)
+                .WithMany()
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Gate)
+                .WithMany()
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);;
+
             builder.OwnsOne(e => e.LoadingStart, lb =>
             {
                 lb.Property(x => x.Day)
@@ -20,6 +44,10 @@ namespace ScanApp.Infrastructure.Persistence.Configurations
                     .IsRequired();
             }).Navigation(e => e.LoadingStart).IsRequired();
 
+            builder.Property(x => x.LoadingDuration)
+                .IsRequired()
+                .HasConversion(new TimeSpanToStringConverter());
+
             builder.OwnsOne(e => e.ArrivalTimeAtDepot, at =>
             {
                 at.Property(x => x.Day)
@@ -30,9 +58,6 @@ namespace ScanApp.Infrastructure.Persistence.Configurations
                     .HasConversion(new TimeSpanToStringConverter())
                     .IsRequired();
             }).Navigation(e => e.ArrivalTimeAtDepot).IsRequired();
-
-            // todo For now ignoring - modes are still a placeholder.
-            builder.Ignore(x => x.Seasons);
 
             base.Configure(builder);
         }
