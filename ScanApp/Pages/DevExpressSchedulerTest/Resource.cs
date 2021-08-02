1b58f6342ab04498010c3340445c9e8df4a7fd19
Kacper Łukowiak <kacper.lukowiak@hsf-ecommerce.com>
2021-08-02 11:00:37 +0200

    Add DevExpress scheduler with timeline grouping presentation page

---
+++ ScanApp/Pages/DevExpressSchedulerTest/Resource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScanApp.Pages.DevExpressSchedulerTest
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public int? GroupId { get; set; }
+        public string Name { get; set; }
+        public bool IsGroup { get; set; }
+        public string TextCss { get; set; }
+        public string BackgroundCss { get; set; }
+        public override bool Equals(object obj) {
+            Resource resource = obj as Resource;
+            return resource != null && resource.Id == Id;
+        }
+        public override int GetHashCode() {
+            return Id;
+        }
+    }
+}
---
+++ ScanApp/Pages/DevExpressSchedulerTest/ResourceAppointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScanApp.Pages.DevExpressSchedulerTest
+{
+    public static partial class ResourceAppointmentCollection {
+        public class ResourceAppointment {
+            public ResourceAppointment() { }
+            public bool Accepted { get; set; }
+            public int AppointmentType { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public string Caption { get; set; }
+            public string Description { get; set; }
+            public string Location { get; set; }
+            public int? Label { get; set; }
+            public int Status { get; set; }
+            public bool AllDay { get; set; }
+            public string Recurrence { get; set; }
+            public int? DepotId { get; set; }
+        }
+    }
+}
