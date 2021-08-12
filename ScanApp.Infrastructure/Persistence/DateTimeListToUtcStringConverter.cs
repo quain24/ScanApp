1a71929ff48b165d2e83d09a7acea332624a37ec
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-08-12 09:35:10 +0200

    Converter to store exceptions to recurrence pattern in single column.

---
+++ ScanApp.Infrastructure/Persistence/DateTimeListToUtcStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScanApp.Infrastructure.Persistence
+{
+    /// <summary>
+    /// Provides a way to store collection of <see cref="DateTime"/> objects in single database table.<br/>
+    /// Data will be stored in UTC time format <c>(example: 2008-10-31T17:04:32)</c> as comma separated string.<para/>
+    /// Dates which <see cref="DateTime.Kind"/> other than <see cref="DateTimeKind.Utc"/> will cause <see cref="InvalidOperationException"/>.<para/>
+    /// Values returned from database will be converted to UTC.
+    /// </summary>
+    public class DateTimeListToUtcStringConverter : ValueConverter<IEnumerable<DateTime>, string>
+    {
+        /// <summary>
+        /// Creates new instance of <see cref="DateTimeListToUtcStringConverter"/>.
+        /// </summary>
+        public DateTimeListToUtcStringConverter() : base(data => FromCode(data), data => FromData(data))
+        {
+        }
+
+        private static string FromCode(IEnumerable<DateTime> data) =>
+            string.Join(",", data.Select(x => x.Kind == DateTimeKind.Utc
+                ? x.ToString("s")
+                : throw new InvalidOperationException(
+                    $"All dates being stored must be in UTC format ({nameof(DateTimeKind)} should be set to {nameof(DateTimeKind.Utc)})")));
+
+        private static IEnumerable<DateTime> FromData(string data) =>
+            data.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => DateTime.SpecifyKind(DateTime.Parse(x), DateTimeKind.Utc))
+                .ToList();
+    }
+}
---
+++ ScanApp.Tests/UnitTests/Infrastructure/Persistence/Extensions/DateTimeListToUtcStringConverterTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using ScanApp.Infrastructure.Persistence;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ScanApp.Tests.UnitTests.Infrastructure.Persistence.Extensions
+{
+    public class DateTimeListToUtcStringConverterTests
+    {
+        public ITestOutputHelper Output { get; }
+
+        public DateTimeListToUtcStringConverterTests(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+        [Fact]
+        public void test()
+        {
+            var aa = TimeZoneInfo.Local;
+            var subject = new DateTimeListToUtcStringConverter();
+            Output.WriteLine(aa.BaseUtcOffset.ToString() + "\r\n");
+            Output.WriteLine(aa.GetUtcOffset(DateTime.Now).ToString() + "\r\n");
+            Output.WriteLine(aa.GetUtcOffset(DateTime.Now - TimeSpan.FromDays(0)).ToString() + "\r\n");
+            Output.WriteLine(DateTime.Now.ToUniversalTime().ToString() + "\r\n");
+            Output.WriteLine(DateTime.Now.ToUniversalTime().ToString("s") + "\r\n");
+            string a = subject.ConvertToProvider(new List<DateTime>() { DateTime.Now.ToUniversalTime(), DateTime.UtcNow, new DateTime(2021, 08, 24, 11, 24, 00, DateTimeKind.Utc) }) as string;
+            Output.WriteLine(a);
+
+            var b = subject.ConvertFromProvider(a) as List<DateTime>;
+            b.ForEach(x =>
+            {
+                Output.WriteLine(x.ToString("s"));
+                Output.WriteLine(x.Kind.ToString());
+                Output.WriteLine("\r\n");
+            });
+        }
+    }
+}
