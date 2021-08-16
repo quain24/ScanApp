90867b275a4c4cc60d49cd26799ded1481ffe429
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-08-16 12:21:49 +0200

    Basic testing WiP.

--- ScanApp.Tests/IntegrationTests/Domain/Entities/OccurrenceTests.cs
+++ ScanApp.Tests/IntegrationTests/Domain/Entities/OccurrenceTests.cs
@@ -1,10 +1,9 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Execution;
 using ScanApp.Domain.Entities;
+using ScanApp.Domain.ValueObjects;
 using System;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using FluentAssertions.Execution;
-using ScanApp.Domain.ValueObjects;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -50,5 +49,107 @@ namespace ScanApp.Tests.IntegrationTests.Domain.Entities
             }
         }
 
+        [Fact]
+        public void Will_save_recurring_occurrence()
+        {
+            var start = new DateTime(2021, 01, 21, 11, 45, 00, DateTimeKind.Utc);
+            var end = new DateTime(2021, 01, 21, 12, 45, 00, DateTimeKind.Utc);
+            var pattern = RecurrencePattern.Daily(1, 24);
+            var subject = new OccurrenceFixtures.Occurrence(start, end, pattern);
+
+            using (var ctx = NewStubDbContext)
+            {
+                ctx.Occurrences.Add(subject);
+                ctx.SaveChanges();
+            }
+
+            using var scope = new AssertionScope();
+            using (var cctx = NewStubDbContext)
+            {
+                cctx.Occurrences.Should().HaveCount(1);
+                var result = cctx.Occurrences.FirstOrDefault();
+
+                result.Should().NotBeNull();
+                result.Start.Should().Be(start);
+                result.End.Should().Be(end);
+                result.RecurrencePattern.Should().Be(pattern);
+            }
+        }
+
+        [Fact]
+        public void Will_save_occurrence_and_exception_to_recurrence_rule()
+        {
+            var start = new DateTime(2021, 01, 21, 11, 45, 00, DateTimeKind.Utc);
+            var end = new DateTime(2021, 01, 21, 12, 45, 00, DateTimeKind.Utc);
+            var pattern = RecurrencePattern.Daily(1, 24);
+            var subject = new OccurrenceFixtures.Occurrence(start, end, pattern);
+
+            var excStart = new DateTime(2021, 01, 24, 11, 45, 00, DateTimeKind.Utc);
+            var excEnd = new DateTime(2021, 01, 24, 12, 45, 00, DateTimeKind.Utc);
+            var replacementDate = new DateTime(2021, 01, 24, 12, 45, 00, DateTimeKind.Utc);
+            var exception = new OccurrenceFixtures.Occurrence(excStart, excEnd);
+
+            using (var ctx = NewStubDbContext)
+            {
+                ctx.Add(subject);
+                ctx.SaveChanges();
+                ctx.Add(exception);
+                exception.MarkAsExceptionTo(subject, replacementDate);
+                subject.AddRecurrenceException(exception, replacementDate);
+                ctx.SaveChanges();
+            }
+
+            using var scope = new AssertionScope();
+            using (var cctx = NewStubDbContext)
+            {
+                cctx.Occurrences.Should().HaveCount(2, "there is an occurrence and an exception to it");
+                var result = cctx.Occurrences.AsEnumerable().ElementAt(0);
+                var exc = cctx.Occurrences.AsEnumerable().ElementAt(1);
+
+                result.Should().NotBeNull();
+                exc.Should().NotBeNull();
+                result.Start.Should().Be(start);
+                result.End.Should().Be(end);
+                result.RecurrencePattern.Should().Be(pattern);
+
+                result.RecurrenceExceptions.Should().HaveCount(1)
+                    .And.AllBeEquivalentTo(replacementDate);
+                result.RecurrenceExceptions.First().Kind.Should().Be(DateTimeKind.Utc);
+            }
+        }
+
+        [Fact]
+        public void Will_cascade_delete_to_exceptions()
+        {
+            var start = new DateTime(2021, 01, 21, 11, 45, 00, DateTimeKind.Utc);
+            var end = new DateTime(2021, 01, 21, 12, 45, 00, DateTimeKind.Utc);
+            var pattern = RecurrencePattern.Daily(1, 24);
+            var subject = new OccurrenceFixtures.Occurrence(start, end, pattern);
+
+            var excStart = new DateTime(2021, 01, 24, 11, 45, 00, DateTimeKind.Utc);
+            var excEnd = new DateTime(2021, 01, 24, 12, 45, 00, DateTimeKind.Utc);
+            var replacementDate = new DateTime(2021, 01, 24, 12, 45, 00, DateTimeKind.Utc);
+            var exception = new OccurrenceFixtures.Occurrence(excStart, excEnd);
+
+            using (var ctx = NewStubDbContext)
+            {
+                ctx.Add(subject);
+                ctx.SaveChanges();
+                ctx.Add(exception);
+                exception.MarkAsExceptionTo(subject, replacementDate);
+                subject.AddRecurrenceException(exception, replacementDate);
+                ctx.SaveChanges();
+            }
+
+            using var scope = new AssertionScope();
+
+            using (var cctx = NewStubDbContext)
+            {
+                var mainOccurrence = cctx.Occurrences.First();
+                cctx.Remove(mainOccurrence);
+                cctx.SaveChanges();
+                cctx.Occurrences.Should().BeEmpty();
+            }
+        }
     }
 }
