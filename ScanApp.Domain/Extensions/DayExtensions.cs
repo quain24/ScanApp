ebac65b5a7325bdce492acbdf087da82a47e82b9
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-08-20 11:49:56 +0200

    Add conversions between ScanApp day implementation and MS DayOfWeek implementation.

---
+++ ScanApp.Domain/Extensions/DayExtensions.cs
@@ -0,0 +1,23 @@
+﻿using ScanApp.Domain.Enums;
+using System;
+
+namespace ScanApp.Domain.Extensions
+{
+    public static class DayExtensions
+    {
+        public static DayOfWeek AsMsDayOfWeek(this Day day) =>
+            day switch
+            {
+                Day.Monday => DayOfWeek.Monday,
+                Day.Tuesday => DayOfWeek.Tuesday,
+                Day.Wednesday => DayOfWeek.Wednesday,
+                Day.Thursday => DayOfWeek.Thursday,
+                Day.Friday => DayOfWeek.Friday,
+                Day.Saturday => DayOfWeek.Saturday,
+                Day.Sunday => DayOfWeek.Sunday,
+                _ => throw new ArgumentOutOfRangeException(nameof(day), day, $"Given {nameof(day)} ({(int)day}) value was not defined in " +
+                                                                             $"in '{nameof(Day)}' enumeration or was a set of flags, which are not " +
+                                                                             $"convertible to {nameof(DayOfWeek)} enumeration.")
+            };
+    }
+}
