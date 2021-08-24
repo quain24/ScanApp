d8a787c02940a419a8139fdacc3cf761963fd973
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-08-24 13:43:56 +0200

    Add migrations for Hes departure plans occurrences and companion entities.

--- ScanApp.Infrastructure/Persistence/Migrations/20210824111143_Hes-Departure_plans-Occurrences_base_and_departure_entities.Designer.cs
+++ ScanApp.Infrastructure/Persistence/Migrations/20210824111143_Hes-Departure_plans-Occurrences_base_and_departure_entities.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScanApp.Infrastructure.Persistence;
 
 namespace ScanApp.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210824111143_Hes-Departure_plans-Occurrences_base_and_departure_entities")]
+    partial class HesDeparture_plansOccurrences_base_and_departure_entities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
