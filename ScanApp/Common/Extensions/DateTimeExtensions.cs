534239569c4a9a2350064499faf8f805cb1d5b5e
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-08-25 16:00:49 +0200

    Add converter from normal C# DateTime to Syncfusion date format.

---
+++ ScanApp/Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ScanApp.Common.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToSyncfusionSchedulerDate(this DateTime? date) =>
+            date.HasValue ? ToSyncfusionSchedulerDate(date.Value) : null;
+
+        public static string ToSyncfusionSchedulerDate(this DateTime date)
+        {
+            if (date.Kind != DateTimeKind.Utc)
+                throw new ArgumentException("Conversion can be performed only on dates marked as UTC.");
+
+            var isoDate = date.ToString("S")
+                .Replace("-", "", StringComparison.OrdinalIgnoreCase)
+                .Replace(":", "", StringComparison.OrdinalIgnoreCase);
+            return isoDate + 'Z';
+        }
+    }
+}
