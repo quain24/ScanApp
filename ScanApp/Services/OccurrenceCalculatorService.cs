234b741734638a53e8fe9a18bb6eb953033f6456
Henryk Wiśniewski <henryk.wisniewski@hsf-ecommerce.com>
2021-08-26 11:07:45 +0200

    Move Occ Calc to use static RecurrenceMapper

--- ScanApp/Services/OccurrenceCalculatorService.cs
+++ ScanApp/Services/OccurrenceCalculatorService.cs
@@ -8,12 +8,11 @@ namespace ScanApp.Services
 {
     public class OccurrenceCalculatorService : IOccurrenceCalculatorService
     {
-        private readonly RecurrenceSyncfusionMapper _mapper = new();
         private readonly SfRecurrenceEditor _re = new ();
 
         public List<DateTime> GetOccurrenceDates(RecurrencePattern pattern, DateTime startDate, DateTime endDate, int? maxResultCount = null)
         {
-            return _re.GetRecurrenceDates(startDate, _mapper.ToSyncfusionRule(pattern), null, maxResultCount, endDate);
+            return _re.GetRecurrenceDates(startDate, RecurrenceSyncfusionMapper.ToSyncfusionRule(pattern), null, maxResultCount, endDate);
         }
     }
 }
